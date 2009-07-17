6ea509751a91a2612208ff539a3c2cfb8b0cfb6c
hh <hh@c0adb3ac-85ad-b347-9a16-a660ac379d24>
2009-07-17 06:44:03 +0000

    git-svn-id: svn://svn.hahoyer.dyndns.org/migrep1/trunk/Reni@4379 c0adb3ac-85ad-b347-9a16-a660ac379d24

--- src/HWClassLibrary/Assembly.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using HWClassLibrary.Debug;
-
-[assembly: CLSCompliant(true)]
-//[assembly: DebuggerVisualizer(typeof (Visualizer), Target = typeof (Dumpable), Description = "Dumpable")]
