7ab56d151e28a735ec1fcb88fdd2ca24e901d390
hh <hh@c0adb3ac-85ad-b347-9a16-a660ac379d24>
2011-01-16 20:46:21 +0000

    - "sealed" decoration
    git-svn-id: svn://svn.hahoyer.dyndns.org/migrep1/trunk/Reni@4658 c0adb3ac-85ad-b347-9a16-a660ac379d24

--- src/reni2/FeatureTest/Array.cs
+++ src/reni2/FeatureTest/Array.cs
@@ -1,12 +1,11 @@
-﻿
-using HWClassLibrary.UnitTest;
+﻿using HWClassLibrary.UnitTest;
 
 namespace Reni.FeatureTest.Array
 {
     [TestFixture]
     [Target("(()<*5<*3<*5<*1) dump_print")]
     [Output("array(4 bits,(5,3,5,1))")]
-    public class ArrayFromPieces : CompilerTest
+    public sealed class ArrayFromPieces : CompilerTest
     {
         [Test, Category(UnderConstruction),Explicit]
         public override void Run() { BaseRun(); }
@@ -14,7 +13,7 @@ namespace Reni.FeatureTest.Array
     [TestFixture]
     [Target("(()<*5<*3)<<(()<*5<*1) dump_print")]
     [Output("array(4 bits,(5,3,5,1))")]
-    public class CombineArraysFromPieces : CompilerTest
+    public sealed class CombineArraysFromPieces : CompilerTest
     {
         [Test, Category(UnderConstruction), Explicit]
         public override void Run() { BaseRun(); }
@@ -22,7 +21,7 @@ namespace Reni.FeatureTest.Array
     [TestFixture]
     [Target("(5 type * 5)(arg/\\) array dump_print")]
     [Output("array(4 bits,(0,1,2,3,4))")]
-    public class FromTypeAndFunction : CompilerTest
+    public sealed class FromTypeAndFunction : CompilerTest
     {
         [Test, Category(UnderConstruction), Explicit]
         public override void Run() { BaseRun(); }
