b2f1beeef4d912817c37e158f502f35f04937323
Harald Hoyer <hahoyer@gmail.com>
2011-09-28 08:47:17 +0200

    Bugfix: compile converter only if necessary

--- src/reni2/ReniParser/ExpressionSyntax.cs
+++ src/reni2/ReniParser/ExpressionSyntax.cs
@@ -84,7 +84,7 @@ namespace Reni.ReniParser
 
         internal override Result ObtainResult(ContextBase context, Category category)
         {
-            var trace = ObjectId == -48 && context.ObjectId == 4 && category.HasCode;
+            var trace = ObjectId == -38 && context.ObjectId == 3 && category.HasCode;
             StartMethodDump(trace, context, category);
             try
             {
@@ -139,7 +139,7 @@ namespace Reni.ReniParser
                 Dump("applyResult", applyResult); 
                 BreakExecution();
                 var result = applyResult
-                    .ReplaceArg(searchResult.ConverterResult(category.Typed, context.RefAlignParam))
+                    .ReplaceArg(c=>searchResult.ConverterResult(c, context.RefAlignParam))
                     .ReplaceArg(leftResult);
                 return ReturnMethodDump(result, true);
             }
