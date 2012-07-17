7a6574e6e50f17ce6b9599239c6a1965f66505e2
Harald Hoyer <hahoyer@gmail.com>
2012-07-18 00:58:32 +0200

    Reformatting

--- src/reni2/Sequence/Feature.cs
+++ src/reni2/Sequence/Feature.cs
@@ -1,3 +1,25 @@
+#region Copyright (C) 2012
+
+//     Project Reni2
+//     Copyright (C) 2011 - 2012 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
+#endregion
+
 using HWClassLibrary.Debug;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +29,15 @@ using Reni.Type;
 
 namespace Reni.Sequence
 {
-    internal sealed class Feature : FeatureBase
+    sealed class Feature : FeatureBase
     {
         public Feature(ISequenceOfBitBinaryOperation definable)
             : base(definable) { }
 
-        internal override TypeBase ResultType(int objSize, int argsSize) { return TypeBase.UniqueNumber(Definable.ResultSize(objSize, argsSize)); }
+        internal override TypeBase ResultType(int objSize, int argsSize)
+        {
+            return TypeBase
+                .UniqueNumber(Definable.ResultSize(objSize, argsSize));
+        }
     }
 }
--- src/reni2/Sequence/Sign.cs
+++ src/reni2/Sequence/Sign.cs
@@ -1,3 +1,25 @@
+#region Copyright (C) 2012
+
+//     Project Reni2
+//     Copyright (C) 2011 - 2012 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
+#endregion
+
 using HWClassLibrary.Debug;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +31,15 @@ using Reni.Type;
 
 namespace Reni.Sequence
 {
-    internal sealed class Sign :
+    sealed class Sign :
         SequenceOfBitOperation,
-        ISearchPath<ISearchPath<ISearchPath<IPrefixFeature, SequenceType>,Type.Array>, Bit>,
+        ISearchPath<ISearchPath<ISearchPath<IPrefixFeature, SequenceType>, Type.Array>, Bit>,
         ISequenceOfBitPrefixOperation
     {
         ISearchPath<ISearchPath<IPrefixFeature, SequenceType>, Type.Array> ISearchPath<ISearchPath<ISearchPath<IPrefixFeature, SequenceType>, Type.Array>, Bit>.Convert(Bit type) { return new OperationPrefixFeature(type, this); }
 
         [DisableDump]
-        public string CSharpNameOfDefaultOperation { get { return Name; } }
-
-        [DisableDump]
         string ISequenceOfBitPrefixOperation.DataFunctionName { get { return DataFunctionName; } }
-
-        public Result SequenceOperationResult(Category category, Size objSize) { throw new NotImplementedException(); }
 
         protected override int ResultSize(int objSize, int argSize) { return BitsConst.PlusSize(objSize, argSize); }
     }
