3e6a7ee8424a6b2644c03188f0c1634e6c888e1b
Harald Hoyer <hahoyer@gmail.com>
2012-10-15 09:33:13 +0200

    Formatting

--- src/reni2/Type/TextItemsType.cs
+++ src/reni2/Type/TextItemsType.cs
@@ -1,30 +1,53 @@
+#region Copyright (C) 2012
+
+//     Project Reni2
+//     Copyright (C) 2012 - 2012 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using HWClassLibrary.Debug;
 using Reni.Basics;
 using Reni.Code;
 using Reni.Feature;
 using Reni.ReniParser;
-using Reni.Sequence;
 
 namespace Reni.Type
 {
-    internal sealed class TextItemsType : TagChild<ArrayType>
+    sealed class TextItemsType : TagChild<ArrayType>
     {
         [DisableDump]
         public readonly ISuffixFeature ToNumberOfBaseFeature;
 
         public TextItemsType(ArrayType parent)
-            : base(parent)
-        {
-            ToNumberOfBaseFeature = new ToNumberOfBaseFeature(this);
-        }
+            : base(parent) { ToNumberOfBaseFeature = new ToNumberOfBaseFeature(this); }
 
         [DisableDump]
         protected override string TagTitle { get { return "text_items"; } }
+        [DisableDump]
+        internal override IFeature Feature { get { return Parent.Feature; } }
 
         internal override void Search(SearchVisitor searchVisitor, ExpressionSyntax syntax)
         {
             searchVisitor.Search(this, () => Parent);
-            if (!searchVisitor.IsSuccessFull)
+            if(!searchVisitor.IsSuccessFull)
                 base.Search(searchVisitor, syntax);
         }
 
@@ -59,7 +82,7 @@ namespace Reni.Type
                 );
         }
 
-        private CodeBase DumpPrintCode()
+        CodeBase DumpPrintCode()
         {
             return UniquePointer
                 .ArgCode
