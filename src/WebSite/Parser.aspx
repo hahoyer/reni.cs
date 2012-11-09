e97cce1b70a717c19ac2090bc6d2217336ed09af
Harald Hoyer <hahoyer@gmail.com>
2012-11-09 07:53:34 +0100

    New: prio table management published

---
+++ src/WebSite/Parser.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Parser.aspx.cs" Inherits="WebSite.Parser" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Parser</title>
+</head>
+<body>
+    <form id="form1" method="post" runat="server">
+        <asp:Label ID="DateTimeNow" runat="server" Text="Label"></asp:Label>
+    <table style="height: 283px; width: 776px; margin-bottom: 156px">
+        <tr>
+            <td>
+                <asp:TextBox id="PrioList" runat="server" TextMode="MultiLine" Height="430px" OnTextChanged="OnTextChanged" AutoPostBack="True"/>
+            </td>
+            <td>
+                <asp:TextBox id="PrioTable" runat="server" TextMode="MultiLine" Height="430px" Width="594px" />
+            </td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ src/WebSite/Parser.aspx.cs
@@ -0,0 +1,41 @@
+﻿#region Copyright (C) 2012
+
+//     Project WebSite
+//     Copyright (C) 2012 - 2012 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using HWClassLibrary.Debug;
+using HWClassLibrary.Helper;
+using Reni.Parser;
+
+namespace WebSite
+{
+    public partial class Parser : Page
+    {
+        protected void Page_Load(object sender, EventArgs e) { DateTimeNow.Text = DateTime.Now.Format(); }
+        protected void OnTextChanged(object sender, EventArgs e)
+        {
+            PrioTable.Text = Services.FormatPrioTable(PrioList.Text);
+        }
+    }
+}
