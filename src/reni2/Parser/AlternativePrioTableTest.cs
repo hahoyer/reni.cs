b4439c6a005dd218edef1923e36c58094e4b5051
hahoyer <hahoyer@gmail.com>
2014-10-18 02:54:25 +0200

    New: Subparsing support

---
+++ src/reni2/Parser/AlternativePrioTableTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using hw.UnitTest;
+using Reni.FeatureTest;
+using Reni.FeatureTest.Helper;
+
+namespace Reni.Parser
+{
+    [TestFixture]
+    [ParserTest]
+    public sealed class AlternativePrioTableTest : CompilerTest
+    {
+        [Test]
+        public void AlternativePrioTableConverter()
+        {
+            var syntaxPrototype = LikeSyntax.Struct
+                (
+                    new[] {LikeSyntax.Number(3)},
+                    new Declaration[] {},
+                    new[] {0}
+                );
+            Parameters.ParseOnly = true;
+            CreateFileAndRunCompiler
+                ("UseAlternativePrioTable", @"!converter: 3", expectedResult: c => syntaxPrototype.AssertLike(c.Syntax));
+        }
+
+        [Test]
+        public void AlternativePrioTableConverterAndProperty()
+        {
+            var syntaxPrototype = LikeSyntax.Struct
+                (                              
+                    new[] {LikeSyntax.Number(3), LikeSyntax.Number(4)},
+                    new Declaration[] {},
+                    new[] {0, 1}
+                );
+            Parameters.ParseOnly = true;
+            CreateFileAndRunCompiler
+                (
+                    "UseAlternativePrioTable",
+                    @"!converter: 3; !converter: 4",
+                    expectedResult: c => syntaxPrototype.AssertLike(c.Syntax));
+        }
+    }
+}
