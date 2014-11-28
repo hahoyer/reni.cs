db86b35cd5b976e0288f48fdaea21729584724d0
hahoyer <hahoyer@gmail.com>
2014-11-28 23:14:30 +0100

    Change: compiling structure access

--- src/reni2/FeatureTest/TypeType/TypeOperatorWithVariable.cs
+++ src/reni2/FeatureTest/TypeType/TypeOperatorWithVariable.cs
@@ -1,29 +1,6 @@
-#region Copyright (C) 2012
-
-//     Project Reni2
-//     Copyright (C) 2012 - 2012 Harald Hoyer
-// 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//     
-//     Comments, bugs and suggestions to hahoyer at yahoo.de
-
-#endregion
-
 using System.Linq;
 using System.Collections.Generic;
 using System;
-using hw.Debug;
 using hw.UnitTest;
 using Reni.FeatureTest.Structure;
 
@@ -31,13 +8,9 @@ namespace Reni.FeatureTest.TypeType
 {
     [TestFixture]
     [InnerAccess]
+    [SomeVariables]
+    [TypeOperator]
+    [TargetSet("x: 0; x type dump_print", "number(bits:1)")]
     public sealed class TypeOperatorWithVariable : CompilerTest
-    {
-        protected override string Target { get { return @"x: 0; x type dump_print"; } }
-        protected override string Output { get { return "(bit)sequence(1)"; } }
-        protected override IEnumerable<System.Type> DependsOn { get { return new[] {typeof(SomeVariables), typeof(TypeOperator)}; } }
-
-        [Test]
-        public override void Run() { BaseRun(); }
-    }
+    {}
 }
--- src/reni2/Struct/Context.cs
+++ src/reni2/Struct/Context.cs
@@ -5,10 +5,14 @@ using hw.Debug;
 using hw.Forms;
 using Reni.Basics;
 using Reni.Context;
+using Reni.Feature;
+using Reni.TokenClasses;
 
 namespace Reni.Struct
 {
-    sealed class Context : Child
+    sealed class Context 
+        : Child
+        , ISymbolProvider<Definable, IFeatureImplementation>
     {
         [Node]
         internal readonly int Position;
@@ -24,6 +28,12 @@ namespace Reni.Struct
 
         [DisableDump]
         internal Structure Structure { get { return Parent.UniqueStructure(Container, Position); } }
+
+        IFeatureImplementation ISymbolProvider<Definable, IFeatureImplementation>.Feature(Definable tokenClass)
+        {
+            var structurePosition = Container.Find(tokenClass.Name);
+            return structurePosition == null ? null : structurePosition.Convert(Structure);
+        }
 
         internal Result ObjectResult(Category category) { return Structure.StructReferenceViaContextReference(category); }
 
