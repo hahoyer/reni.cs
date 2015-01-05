96bb6fdf618e1bccf886fd49046573466505feeb
hahoyer <hahoyer@gmail.com>
2015-01-05 17:26:37 +0100

    Test

--- src/reni2/FeatureTest/Function/SimpleFunction.cs
+++ src/reni2/FeatureTest/Function/SimpleFunction.cs
@@ -8,13 +8,11 @@ using Reni.FeatureTest.Structure;
 namespace Reni.FeatureTest.Function
 {
     [TestFixture]
-    [TargetSet(@"f: /\ . + 1;f(2) dump_print;", "3")]
+    [TargetSet(@"f: /\ ^ + 1;f(2) dump_print;", "3")]
     [InnerAccess]
     [Add2Numbers]
     [Function]
     public sealed class SimpleFunction : CompilerTest
     {
-        [Test]
-        public override void Run() { BaseRun(); }
     }
 }
