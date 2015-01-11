d25140cfc0343542bd2b3ed8cb57618aaf498cf3
hahoyer <hahoyer@gmail.com>
2015-01-11 17:54:20 +0100

    Test

--- src/reni2/FeatureTest/ConversionService/Closure.cs
+++ src/reni2/FeatureTest/ConversionService/Closure.cs
@@ -17,25 +17,27 @@ namespace Reni.FeatureTest.ConversionService
             var number = new Root(null).BitType.Number(1);
             var types = number.SymmetricFeatureClosure().Types().ToArray();
 
-            Tracer.Assert(types.Length == 3);
             Tracer.Assert(types.Contains(number));
             Tracer.Assert(types.Contains(number.Align));
             Tracer.Assert(types.Contains(number.Pointer));
+            Tracer.Assert(types.Length == 3);
         }
+
         [Test]
         public void OfNumberPointer()
         {
             var number = new Root(null).BitType.Number(4);
             var pointer = number.Pointer;
             var paths = pointer.SymmetricPathsClosure().ToArray();
+
+            Tracer.Assert(paths.Select(p => p.Source).Distinct().Single() == pointer);
+
             var destinations = paths.Select(p => p.Destination).ToArray();
 
-            Tracer.Assert(paths.Length == 3);
-            Tracer.Assert(paths.Select(p => p.Source).Distinct().Single() == pointer);
-            Tracer.Assert(destinations.Length == 3);
             Tracer.Assert(destinations.Contains(number.Pointer));
             Tracer.Assert(destinations.Contains(number.Align));
             Tracer.Assert(destinations.Contains(number));
+            Tracer.Assert(destinations.Length == 3);
         }
     }
 }
