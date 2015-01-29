9c5c2567b8d3d1b512329ef3804c65d824406cff
hahoyer <hahoyer@gmail.com>
2015-01-29 01:04:40 +0100

    Change: Replace context reference by object pointer in case of left site is a compound object

--- src/reni2/Context/FeatureDescriptor.cs
+++ src/reni2/Context/FeatureDescriptor.cs
@@ -24,12 +24,12 @@ namespace Reni.Context
             if(metaFeature != null)
                 return metaFeature.Result(context, category, left, right);
 
-            var trace = ObjectId == 46 && category.HasCode;
-            StartMethodDump(trace, context, category, left, right,null);
+            var trace = ObjectId == 92 && category.HasCode;
+            StartMethodDump(trace, category, context, left, right,null);
             try
             {
                 BreakExecution();
-                var rawResult = Result(context, category, right);
+                var rawResult = Result(category, context, right);
                 Dump("rawResult", rawResult);
                 BreakExecution();
 
@@ -37,7 +37,17 @@ namespace Reni.Context
                 Dump("converterResult", converterResultForAll);
                 BreakExecution();
 
-                var resultWithConversions = rawResult.ReplaceArg(converterResult);
+                var rawResult2 = rawResult;
+                var compoundType = Type.AutomaticDereferenceType as CompoundType;
+                if(compoundType != null)
+                {
+                    rawResult2 = rawResult.ContextViaObjectPointer(compoundType.View);
+                    Dump("rawResult2", rawResult2);
+                    BreakExecution();
+                }
+
+                var resultWithConversions = rawResult2
+                    .ReplaceArg(converterResult);
                 Dump("resultWithConversions", resultWithConversions);
                 BreakExecution();
 
@@ -60,16 +70,16 @@ namespace Reni.Context
             if(metaFeature != null)
                 return metaFeature.Result(context, category, right);
 
-            var result = Result(context, category, right);
+            var result = Result(category, context, right);
             return result.ReplaceArg(converterResult);
         }
 
         internal Result Result(Category category, Func<Category, Result> converterResult) => SimpleResult(category).ReplaceArg(converterResult);
 
-        Result Result(ContextBase context, Category category, CompileSyntax right)
+        Result Result(Category category, ContextBase context, CompileSyntax right)
         {
-            var trace = ObjectId.In(730) && category.HasCode;
-            StartMethodDump(trace, context, category, right);
+            var trace = ObjectId.In(-92) && category.HasCode;
+            StartMethodDump(trace, category, context, right);
             try
             {
                 var simpleFeature = Feature.SimpleFeature();
