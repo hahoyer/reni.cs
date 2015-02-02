3db8c04ecbadd1277e60f5b9fd71b56d423834cf
hahoyer <hahoyer@gmail.com>
2015-02-02 10:14:38 +0100

    Change: EnableReassign for use with array type implemented

--- src/reni2/TokenClasses/ReassignToken.cs
+++ src/reni2/TokenClasses/ReassignToken.cs
@@ -3,9 +3,12 @@ using System.Linq;
 using System;
 using hw.Debug;
 using hw.Scanner;
+using Reni.Basics;
+using Reni.Context;
 using Reni.Feature;
 using Reni.ReniParser;
 using Reni.ReniSyntax;
+using Reni.Type;
 
 namespace Reni.TokenClasses
 {
@@ -20,7 +23,10 @@ namespace Reni.TokenClasses
     sealed class EnableReassignToken : TokenClass
     {
         public const string Id = ":=!";
-        protected override Syntax Prefix(SourcePart token, Syntax right) => new EnableReassignSyntax(token, right.ToCompiledSyntax);
+        protected override Syntax Prefix(SourcePart token, Syntax right)
+            => new EnableReassignSyntax(token, right.ToCompiledSyntax);
+        protected override Syntax Suffix(Syntax left, SourcePart token)
+            => new EnableReassignTypeSyntax(left.ToCompiledSyntax, token);
     }
 
     sealed class EnableReassignSyntax : Syntax
@@ -35,5 +41,24 @@ namespace Reni.TokenClasses
         protected override string GetNodeDump() => base.GetNodeDump() + "(" + _target.NodeDump + ")";
         internal override CompileSyntax ContainerStatementToCompileSyntax => _target.ContainerStatementToCompileSyntax;
         internal override bool IsMutableSyntax => true;
+    }
+
+    sealed class EnableReassignTypeSyntax : CompileSyntax
+    {
+        readonly CompileSyntax _target;
+        public EnableReassignTypeSyntax(CompileSyntax target, SourcePart token)
+            : base(token)
+        {
+            _target = target;
+        }
+
+        protected override string GetNodeDump() => "(" + _target.NodeDump + ")" + base.GetNodeDump();
+
+        internal override Result ResultForCache(ContextBase context, Category category)
+        {
+            var leftType = context.Type(_target);
+            var leftTypeType = ((TypeType) leftType).Value;
+            return ((ArrayType) leftTypeType).Mutable.TypeType.Result(category);
+        }
     }
 }
