ddf80fb6df035fdb6296148adfe5f76a70e87b83
hahoyer <hahoyer@gmail.com>
2015-02-11 21:28:00 +0100

    Bug: Lists with empty elements are not compiled

--- src/reni2/TokenClasses/List.cs
+++ src/reni2/TokenClasses/List.cs
@@ -9,13 +9,16 @@ namespace Reni.TokenClasses
 {
     sealed class List : TokenClass
     {
+        protected override Syntax Prefix(SourcePart token, Syntax right)
+            => ListSyntax(new EmptyList(token), token, right);
+
         protected override Syntax Suffix(Syntax left, SourcePart token)
-        {
-            return new ListSyntax(this, token, left.ToList(this));
-        }
+            => ListSyntax(left, token, new EmptyList(token));
+
         protected override Syntax Infix(Syntax left, SourcePart token, Syntax right)
-        {
-            return new ListSyntax(this, token, left.ToList(this).Concat(right.ToList(this)).ToArray());
-        }
+            => ListSyntax(left, token, right);
+
+        ListSyntax ListSyntax(Syntax left, SourcePart token, Syntax right)
+            => new ListSyntax(this, token, left.ToList(this).Concat(right.ToList(this)).ToArray());
     }
 }
