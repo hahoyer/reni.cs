634fb4e3bfa3350aebd8f81297481a813f763885
hahoyer <hahoyer@gmail.com>
2015-03-03 19:31:48 +0100

    Debug

--- src/reni2/Code/TopData.cs
+++ src/reni2/Code/TopData.cs
@@ -30,7 +30,7 @@ namespace Reni.Code
         public TopFrameData(Size offset, Size size, Size dataSize)
             : base(offset, size, dataSize)
         {
-            StopByObjectIds(202);
+            StopByObjectIds();
         }
 
         protected override CodeBase TryToCombine(FiberItem subsequentElement)
