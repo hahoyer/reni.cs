ceb4161cb2d08d6ab398f0f9e352048138465398
hahoyer <hahoyer@gmail.com>
2015-03-05 09:23:53 +0100

    Test: complex (real+imaginary) object

--- src/reni2/FeatureTest/UserObjects.cs
+++ src/reni2/FeatureTest/UserObjects.cs
@@ -14,15 +14,30 @@ system: /!\
 . MaxNumber64: /!\ '7fffffffffffffff' to_number_of_base 16 
 };
 
-o: /\ 
+complex: 
 {
-    re: system MaxNumber32 instance(^);
-    im: system MaxNumber32 instance(0)
+    FromReal: /\ Create(^;0);
+
+    Create: /\
+    {
+        re: system MaxNumber8 type instance((^_A_T_ 0) enable_cut);
+        im: system MaxNumber8 type instance((^_A_T_ 1) enable_cut);
+        + : /\ complex Create(re + ^re, im + ^im);
+        * : /\ complex Create(re * ^re - im * ^im, re * ^im + im * ^re);
+        dump_print: /!\
+        (
+            re dump_print;
+            '+' dump_print;
+            im dump_print;
+            'i' dump_print
+        )
+    }
 };
 
-o (2) dump_print
-
-
-", "(2,0)")]
-    public class UserObjects : CompilerTest {}
+complex FromReal(2) dump_print;
+' ' dump_print;
+(complex Create(0,1) * complex Create(0,1)) dump_print
+", 
+        "2+0i -1+0i")]
+    public sealed class UserObjects : CompilerTest {}
 }
