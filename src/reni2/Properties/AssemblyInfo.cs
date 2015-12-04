c553efa626f04c9e3ccef055bf2f20d8c63b3d5f
hahoyer <hahoyer@gmail.com>
2015-12-04 19:59:27 +0100

    Change: Identify remaining externals

---
+++ src/reni2/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("15.0.0.0")]
+[assembly: AssemblyFileVersion("15.0.0.0")]
+[assembly: AssemblyTitle("Reni compiler")]
+[assembly: AssemblyCompany("Hoyerware")]
+[assembly: AssemblyProduct("Reni.Properties")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2015")]
+[assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("ReniTest")]
