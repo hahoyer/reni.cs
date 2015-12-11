4b71104623e10827ea3fd20b655cf982c6a64476
hahoyer <hahoyer@gmail.com>
2015-12-11 20:54:03 +0100

    Bug: Application did not end

--- src/ReniTest/CompilationView/MainView.cs
+++ src/ReniTest/CompilationView/MainView.cs
@@ -2,17 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using Reni.Struct;
 
 namespace ReniTest.CompilationView
 {
     abstract class MainView : View
     {
         protected MainView(string name)
-            : base(name) { }
+            : base(name) { Frame.FormClosing += (a, b) => Application.Exit(); }
 
         internal void Register(Form child) { Frame.Closing += (a, s) => child.Close(); }
         internal void Run() => Application.Run(Frame);
-
     }
 }
