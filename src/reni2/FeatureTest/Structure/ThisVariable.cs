6cf76052a87c49281a6c6b2ed4964bb4f16bdc33
hahoyer <hahoyer@gmail.com>
2016-01-06 09:50:38 +0100

    Test

--- src/reni2/FeatureTest/Structure/ThisVariable.cs
+++ src/reni2/FeatureTest/Structure/ThisVariable.cs
@@ -11,7 +11,7 @@ namespace Reni.FeatureTest.Structure
     [TargetSet(@"
 x: /\ 
 {
-  this: /!\ ^^ reference;
+  this: /!\ ^^ stable_reference;
   xxx: 12;
   result: this xxx; 
 },
@@ -19,5 +19,40 @@ x: /\
 x()result dump_print
 
 ", "12")]
-    public sealed class ThisVariable : CompilerTest {}
+    public sealed class ThisVariable0 : CompilerTest {}
+
+    [UnitTest]
+    [Access]
+    [TargetSet(@"
+x: /\ 
+{
+  this: /!\ ^^ stable_reference;
+  !mutable xxx: 12;
+  result: this xxx; 
+};
+
+xx : x();
+xx this xxx := 2;
+
+x()result dump_print
+
+", "2")]
+    public sealed class ThisVariable1 : CompilerTest { }
+
+
+    [UnitTest]
+    [ThisVariable0]
+    [ThisVariable1]
+    [TargetSet(@"
+x: /\ 
+{
+  this: /!\ ^^ stable_reference;
+  xxx: 12;
+  result: this xxx; 
+} result ,
+
+x() dump_print
+
+", "12")]
+    public sealed class ThisVariable : CompilerTest { }
 }
