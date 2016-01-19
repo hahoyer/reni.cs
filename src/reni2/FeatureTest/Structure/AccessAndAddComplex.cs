b433414c227d5354a2c27e2b22b264f56256a7ec
hahoyer <hahoyer@gmail.com>
2016-01-19 09:34:33 +0100

    Test

--- src/reni2/FeatureTest/Structure/AccessAndAddComplex.cs
+++ src/reni2/FeatureTest/Structure/AccessAndAddComplex.cs
@@ -11,7 +11,7 @@ namespace Reni.FeatureTest.Structure
     [AccessAndAdd]
     [BitArrayOp.BitArrayOp]
     [SubtractOddSizedNumber]
-    [TargetSet(" 1; 4;2050; (^^ _A_T_ 0) + (^^ _A_T_ 1) + (^^ _A_T_ 2);(^^ _A_T_ 3) dump_print;", "2055")]
+    [TargetSet(" 1; 4;2050; (_A_T_ 0) + (_A_T_ 1) + (_A_T_ 2);(_A_T_ 3) dump_print;", "2055")]
     public sealed class AccessAndAddComplex : CompilerTest
     {}
 }
--- src/reni2/FeatureTest/Structure/SimpleAssignment.cs
+++ src/reni2/FeatureTest/Structure/SimpleAssignment.cs
@@ -8,7 +8,7 @@ namespace Reni.FeatureTest.Structure
 {
     [UnitTest]
     [InnerAccess]
-    [TargetSet(@"(!mutable: 10, (^^ _A_T_ 0) := 4) dump_print", "(4, )")]
+    [TargetSet(@"(!mutable: 10, (_A_T_ 0) := 4) dump_print", "(4, )")]
     public sealed class SimpleAssignment : CompilerTest
     {}
 }
