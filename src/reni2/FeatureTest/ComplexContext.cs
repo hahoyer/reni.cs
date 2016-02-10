d389c1a95f587ef5ef49e70839b4218a0ca3b9b2
hahoyer <hahoyer@gmail.com>
2016-02-10 09:57:38 +0100

    Tests

--- src/reni2/FeatureTest/ComplexContext.cs
+++ src/reni2/FeatureTest/ComplexContext.cs
@@ -8,7 +8,7 @@ using Reni.FeatureTest.Helper;
 namespace Reni.FeatureTest
 {
     [UnitTest]
-    [Output("Hallo")]
+    [Output("Ha")]
     public sealed class ComplexContext : CompilerTest
     {
         protected override string Target => "FeatureTest\\ComplexContext.reni".FileHandle().String;
