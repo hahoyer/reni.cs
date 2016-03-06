b7743c23157f98e725eda9683f98987e790ce89b
hahoyer <hahoyer@gmail.com>
2016-03-06 15:31:00 +0100

    Admin

--- src/ReniUI/Formatting/IFormatter.cs
+++ src/ReniUI/Formatting/IFormatter.cs
@@ -18,7 +18,41 @@ namespace ReniUI.Formatting
 
         public static string Reformat(this SourceSyntax syntax, SourcePart sourcePart, IFormatter formatter = null)
             =>
-                (formatter ?? new Formatting.Configuration().Create()).Reformat
+                (formatter ?? new Configuration().Create()).Reformat
                     (syntax, sourcePart);
+
+        internal static int Length(this LineOrientedFormatter.Item2 item)
+        {
+            var result = item.NewHeader.Length;
+            if(item.IsRelevant)
+                result += item.Part.Length;
+            return result;
+        }
+
+        internal static string Filter
+            (this IEnumerable<LineOrientedFormatter.Line> rawLines, SourcePart part)
+        {
+            var result = "";
+            var lines = rawLines
+                .Select(line => line.Data.Where(item => item.Contains(part)))
+                .SelectMany(items => items.Where(item => item.IsRelevant));
+
+            foreach(var item in lines)
+                result += item.NewValue(part);
+            return result;
+        }
+
+        static string NewValue(this LineOrientedFormatter.Item2 item, SourcePart part)
+        {
+            var subResult = "";
+            if(item.Part.Position >= part.Position)
+                subResult += item.NewHeader;
+            subResult += part.Intersect(item.Part).Id;
+            return subResult;
+        }
+
+        internal static bool Contains(this LineOrientedFormatter.Item2 item, SourcePart part)
+            => item.Part.Position < part.EndPosition
+                && item.Part.EndPosition > part.Position;
     }
 }
