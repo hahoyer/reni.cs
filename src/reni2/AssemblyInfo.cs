fc876e4c5371a3d12f34bcf3a6e7d1b3fce5295a
hahoyer <hahoyer@gmail.com>
2016-03-06 01:16:53 +0100

    Admin

---
+++ src/reni2/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("16.1.0.0")]
+[assembly: AssemblyFileVersion("16.1.0.0")]
+[assembly: AssemblyTitle("Reni Compiler")]
+[assembly: AssemblyCompany("HoyerWare")]
+[assembly: AssemblyProduct("Reni Compiler")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2016")]
+[assembly: ComVisible(false)]
+
+[assembly: InternalsVisibleTo("ReniBrowser")]
