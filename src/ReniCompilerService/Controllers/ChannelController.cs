0476481fbe8ae1493ffdadc84eb73fc2544a3e00
hahoyer <hahoyer@gmail.com>
2016-03-14 00:06:08 +0100

    New: restFul

---
+++ src/ReniCompilerService/Controllers/ChannelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using ReniUI.RestFul;
+
+namespace ReniCompilerService.Controllers
+{
+    [Route("Channel")]
+    public sealed class ChannelController : Controller
+    {
+        [HttpGet]
+        public string Get() => Repo.AddAndGetNewId();
+
+        [HttpGet("{id}")]
+        public Channel Get(string id) => Repo.Find(id);
+
+        [HttpGet("{id}/Result")]
+        public string GetResult(string id) => Repo.Find(id).GetResult();
+
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Channel value) => Repo.Update(id,value);
+
+        [HttpDelete("{id}")]
+        public void Delete(string id) => Repo.Remove(id);
+
+        [FromServices]
+        public IChannelRepo Repo { get; set; }
+    }
+}
---
+++ src/ReniCompilerService/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace ReniCompilerService.Controllers
+{
+    [Route("welcome")]
+    public sealed class WelcomeController : Controller
+    {
+        [HttpGet]
+        public string Get() => "Welcome to reni compiler service.";
+    }
+}
