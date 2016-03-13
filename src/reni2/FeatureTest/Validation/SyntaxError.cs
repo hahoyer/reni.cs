077a61ec7b4f87fc72d43f4dc9fd1bbcd2841efa
hahoyer <hahoyer@gmail.com>
2016-03-13 23:52:24 +0100

    Test: Filepos is region now

--- src/reni2/FeatureTest/Validation/SyntaxError.cs
+++ src/reni2/FeatureTest/Validation/SyntaxError.cs
@@ -1,11 +1,12 @@
-using System.Linq;
-using System.Collections.Generic;
 using System;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 using hw.DebugFormatter;
 using hw.Helper;
+using hw.Scanner;
 using hw.UnitTest;
 using Reni.FeatureTest.Helper;
+using Reni.Parser;
 using Reni.Validation;
 
 namespace Reni.FeatureTest.Validation
@@ -18,13 +19,11 @@ namespace Reni.FeatureTest.Validation
     {
         protected override void Verify(IEnumerable<Issue> issues)
         {
-            var issueArray = issues.ToArray();
+            var a = issues.ToArray();
             var i = 0;
             Tracer.Assert
-                (
-                    issueArray[i++].IsLogdumpLike
-                        (1, 3, IssueId.EOFInComment, "#(x asdf y)# dump_print"));
-            Tracer.Assert(issueArray.Length == i);
+                (a[i++].IsLogdumpLike(1, 3, 1, 26, IssueId.EOFInComment, "#(x asdf y)# dump_print"));
+            Tracer.Assert(a.Length == i);
         }
     }
 
@@ -36,39 +35,51 @@ world'
     [Output("")]
     public sealed class SyntaxErrorString : CompilerTest
     {
-        public SyntaxErrorString()
-        {
-            // Parameters.TraceOptions.Parser = true;
-        }
         protected override void Verify(IEnumerable<Issue> issues)
         {
-            var issueArray = issues.ToArray();
+            var a = issues.ToArray();
             var i = 0;
-            Tracer.Assert(issueArray[i++].IsLogdumpLike(2, 1, IssueId.EOLInString, "' hallo"));
-            Tracer.Assert(issueArray[i++].IsLogdumpLike(3, 6, IssueId.EOLInString, "'"));
-            Tracer.Assert(issueArray[i++].IsLogdumpLike(3, 1, IssueId.MissingDeclaration, "world Type: ()"));
-            Tracer.Assert(issueArray.Length == i);
+            Tracer.Assert(a[i++].IsLogdumpLike(2, 1, 3, 1, IssueId.EOLInString, "' hallo"));
+            Tracer.Assert(a[i++].IsLogdumpLike(3, 6, 4, 1, IssueId.EOLInString, "'"));
+            Tracer.Assert
+                (a[i++].IsLogdumpLike(3, 1, 3, 6, IssueId.MissingDeclaration, "world Type: ()"));
+            Tracer.Assert(a.Length == i);
         }
     }
 
     static class SyntaxErrorExtension
     {
-        const string Pattern = ".*\\.reni\\({0},{1}\\): error {2}: (.*)";
+        const string Pattern = ".reni({0},{1},{2},{3}): error {4}: ";
+
+        const string RegExPattern = ".*\\.reni\\({0},{1}\\): error {2}: (.*)";
 
         internal static bool IsLogdumpLike
-            (this Issue target, int line, int column, IssueId issueId, string text)
+            (
+            this Issue target,
+            int line,
+            int column,
+            int lineEnd,
+            int columnEnd,
+            IssueId issueId,
+            string expectedText)
         {
             if(target.IssueId != issueId)
                 return false;
 
             var logDump = target.LogDump;
-            var value =
-                new Regex(Pattern.ReplaceArgs(line, column, issueId)).Match
-                    (logDump.Replace("\r", "")).Groups[1]
-                    .Value;
-            if(value != text)
+
+            var pattern = Pattern.ReplaceArgs(line, column, lineEnd, columnEnd, issueId);
+            var match = pattern.Box().Find;
+
+            var start = match.Apply(logDump);
+            if(start == null)
                 return false;
-            return true;
+
+            var logText = logDump.Substring(start.Value);
+            if (logText.StartsWith(expectedText))
+                return true;
+
+            return false;
         }
     }
 }
