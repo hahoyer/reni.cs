6d81dc0c87c73016368271004a6a0dc6ce59b540
hahoyer <hahoyer@gmail.com>
2016-05-17 09:59:25 +0200

    Test

--- src/ReniUI/Test/AutoCompleteFunctionInCompound.cs
+++ src/ReniUI/Test/AutoCompleteFunctionInCompound.cs
@@ -9,6 +9,7 @@ namespace ReniUI.Test
 {
     [UnitTest]
     [AutoCompleteSimple]
+    [LowPriority]
     public sealed class AutoCompleteFunctionInCompound : DependantAttribute
     {
         const string text = @"
@@ -23,15 +24,12 @@ namespace ReniUI.Test
         public void GetDeclarationOptions()
         {
             var compiler = CompilerBrowser.FromText(text);
-            for (var i = 0; i < text.Length; i++)
+            var offset = 64;
+            var position = compiler.Source + offset;
+            if((position + -1).Span(2).Id != "\r\n")
             {
-                var offset = text.Length - i - 1;
-                var position = compiler.Source + offset;
-                if ((position + -1).Span(2).Id != "\r\n")
-                {
-                    var t = compiler.DeclarationOptions(offset);
-                    Tracer.Assert(t != null, () => (new Source(text) + i).Dump());
-                }
+                var t = compiler.DeclarationOptions(offset);
+                Tracer.Assert(t != null, () => (new Source(text) + offset).Dump());
             }
         }
     }
