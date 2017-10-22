c33e28238d581eb1f93867bcfa6c7eaf07e4fb41
hahoyer.Anja <hahoyer@gmail.com>
2017-10-22 23:11:31 +0200

    Refactor

--- src/reni2/Struct/SetterFunctionType.cs
+++ src/reni2/Struct/SetterFunctionType.cs
@@ -1,29 +1,3 @@
-#region Copyright (C) 2012
-
-//     Project Reni2
-//     Copyright (C) 2012 - 2012 Harald Hoyer
-// 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//     
-//     Comments, bugs and suggestions to hahoyer at yahoo.de
-
-#endregion
-
-using System.Linq;
-using System.Collections.Generic;
-using System;
-using hw.DebugFormatter;
 using Reni.Basics;
 using Reni.Context;
 using Reni.Parser;
@@ -33,15 +7,12 @@ namespace Reni.Struct
 {
     sealed class SetterFunction : FunctionInstance
     {
-        readonly FunctionId _functionId;
         public SetterFunction(FunctionType parent, int index, Value body)
-            : base(parent, body)
-        {
-            _functionId = FunctionId
-                .Setter(index);
-        }
+            : base(parent, body) => FunctionId = FunctionId
+            .Setter(index);
 
-        protected override FunctionId FunctionId => _functionId;
+        protected override FunctionId FunctionId { get; }
+
         protected override TypeBase CallType => base.CallType.Pair(Parent.ValueType.Pointer);
         protected override Size RelevantValueSize => Root.DefaultRefAlignParam.RefSize;
     }
