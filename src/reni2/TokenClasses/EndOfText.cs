d7f187569a4d6d8a214ea79b614c2aa4af3af857
hahoyer.Anja <hahoyer@gmail.com>
2017-11-01 17:52:51 +0100

    Change: Do not remove Begin/End-of-text level

--- src/reni2/TokenClasses/EndOfText.cs
+++ src/reni2/TokenClasses/EndOfText.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using hw.DebugFormatter;
+﻿using hw.DebugFormatter;
 using hw.Parser;
 using Reni.Parser;
 
 namespace Reni.TokenClasses
 {
-    sealed class EndOfText : TokenClass, IDefaultScopeProvider, IBracketMatch<Syntax>
+    sealed class EndOfText : TokenClass, IDefaultScopeProvider, IBracketMatch<Syntax>, IStatementsProvider
     {
-        const string TokenId = PrioTable.EndOfText;
-        [DisableDump]
-        public override string Id => TokenId;
-        [DisableDump]
-        internal override bool IsVisible => false;
-        bool IDefaultScopeProvider.MeansPublic => true;
-        IParserTokenType<Syntax> IBracketMatch<Syntax>.Value { get; } = new Matched();
-
         sealed class Matched : DumpableObject, IParserTokenType<Syntax>
         {
             Syntax IParserTokenType<Syntax>.Create(Syntax left, IToken token, Syntax right)
@@ -24,16 +13,43 @@ namespace Reni.TokenClasses
                 Tracer.Assert(right == null);
                 Tracer.Assert(left.Right == null);
                 Tracer.Assert(left.Left.Left == null);
-                return left.Left.Right;
+                return left;
             }
 
             string IParserTokenType<Syntax>.PrioTableId => "()";
         }
+
+        const string TokenId = PrioTable.EndOfText;
+        IParserTokenType<Syntax> IBracketMatch<Syntax>.Value {get;} = new Matched();
+        bool IDefaultScopeProvider.MeansPublic => true;
+
+        Result<Statement[]> IStatementsProvider.Get(List type, Syntax right, IDefaultScopeProvider container)
+        {
+            Tracer.Assert(type == null);
+            Tracer.Assert(right != null);
+
+            Tracer.Assert(right.Left != null);
+            Tracer.Assert(right.TokenClass is EndOfText);
+            Tracer.Assert(right.Right == null);
+
+            Tracer.Assert(right.Left.Left == null);
+            Tracer.Assert(right.Left.TokenClass is BeginOfText);
+
+            var result = right.Left.Right;
+            return result?.GetStatements() ?? new Result<Statement[]>(new Statement[0]);
+        }
+
+        [DisableDump]
+        public override string Id => TokenId;
+
+        [DisableDump]
+        internal override bool IsVisible => false;
     }
 
     sealed class BeginOfText : TokenClass
     {
         const string TokenId = PrioTable.BeginOfText;
+
         [DisableDump]
         public override string Id => TokenId;
     }
