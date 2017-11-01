be3d2f03b9debfca77e7974267f4439550d92b60
hahoyer.Anja <hahoyer@gmail.com>
2017-11-02 00:53:59 +0100

    Tests

--- src/ReniUI/Test/Formatting.cs
+++ src/ReniUI/Test/Formatting.cs
@@ -11,49 +11,6 @@ using ReniUI.Formatting;
 namespace ReniUI.Test
 {
     [UnitTest]
-    [TestFixture]       
-    public sealed class FormattingSimple : DependantAttribute
-    {
-        [Test]
-        [UnitTest]
-        public void ReformatComments()
-        {
-            const string Text =
-                @"137;
-
-################################################################
-# Test
-################################################################
-                   3
-";
-
-            const string ExpectedText =
-                @"137;
-
-################################################################
-# Test
-################################################################
-3";
-            var compiler = CompilerBrowser.FromText(Text);
-            var newSource = compiler.Reformat
-                (
-                    new ReniUI.Formatting.Configuration
-                    {
-                        EmptyLineLimit = 0
-                    }
-                        .Create()
-                );
-
-            var lineCount = newSource.Count(item => item == '\n');
-
-            Tracer.Assert
-                (
-                    newSource.Replace("\r\n", "\n") == ExpectedText.Replace("\r\n", "\n"),
-                    "\n\"" + newSource + "\"");
-        }
-    }
-
-    [UnitTest]
     [TestFixture]
     [FormattingSimple]
     public sealed class Formatting : DependantAttribute
---
+++ src/ReniUI/Test/FormattingSimple.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using hw.DebugFormatter;
+using hw.UnitTest;
+using NUnit.Framework;
+using ReniUI.Formatting;
+
+namespace ReniUI.Test {
+    [UnitTest]
+    [TestFixture]       
+    public sealed class FormattingSimple : DependantAttribute
+    {
+        [Test]
+        [UnitTest]
+        public void ReformatComments()
+        {
+            const string Text =
+                @"137;
+
+################################################################
+# Test
+################################################################
+                   3
+";
+
+            const string ExpectedText =
+                @"137;
+
+################################################################
+# Test
+################################################################
+3";
+            var compiler = CompilerBrowser.FromText(Text);
+            var newSource = compiler.Reformat
+            (
+                new ReniUI.Formatting.Configuration
+                    {
+                        EmptyLineLimit = 0
+                    }
+                    .Create()
+            );
+
+            var lineCount = newSource.Count(item => item == '\n');
+
+            Tracer.Assert
+            (
+                newSource.Replace("\r\n", "\n") == ExpectedText.Replace("\r\n", "\n"),
+                "\n\"" + newSource + "\"");
+        }
+    }
+}
