5917b699b713c3b262a54356c20c73c5d7ad0a82
hahoyer.Anja <hahoyer@gmail.com>
2017-11-04 10:20:36 +0100

    Refactor: Reformat

--- src/ReniUI/Formatting/IResultItem.cs
+++ src/ReniUI/Formatting/IResultItem.cs
@@ -2,21 +2,24 @@ using System.Collections.Generic;
 using hw.DebugFormatter;
 using hw.Scanner;
 
-namespace ReniUI.Formatting {
+namespace ReniUI.Formatting
+{
     interface IResultItem
     {
+        [DisableDump]
+        SourcePart SourcePart {get;}
+
+        [DisableDump]
+        string VisibleText {get;}
+
+        [DisableDump]
+        string NewText {get;}
+
+        [DisableDump]
+        int RemoveCount {get;}
+
         IEnumerable<IResultItem> Combine(IResultItem right);
         IEnumerable<IResultItem> CombineBack(WhiteSpace left);
         IEnumerable<IResultItem> CombineBack(NewWhiteSpace left);
-
-        [DisableDump]
-        SourcePart SourcePart { get; }
-
-        [DisableDump]
-        string VisibleText { get; }
-        [DisableDump]
-        string NewText { get; }
-        [DisableDump]
-        int RemoveCount { get; }
     }
 }
