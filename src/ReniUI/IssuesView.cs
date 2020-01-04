ef4f35406ebd6d34f9ea98c34df56e2203436f68
hahoyer.Anja <hahoyer@gmail.com>
2020-01-04 11:40:12 +0100

    Change: Put Issues-configuration to correct place

--- src/ReniUI/IssuesView.cs
+++ src/ReniUI/IssuesView.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using hw.Helper;
 using hw.Scanner;
 using Reni.Validation;
 
@@ -10,7 +11,7 @@ namespace ReniUI
     sealed class IssuesView : ChildView
     {
         public IssuesView(IDataProvider provider)
-            : base(provider.Master, SystemConfiguration.IssuesFilePath)
+            : base(provider.Master, SystemConfiguration.IssuesFilePath.PathCombine("Position"))
         {
             Frame.Text = "Issues";
 
