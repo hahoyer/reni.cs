a5d459c674982065f422fc27817c7c4cf3318497
hahoyer.Anja <hahoyer@gmail.com>
2020-10-06 15:57:02 +0200

    admin

--- src/reni2/AssemblyInfo.cs
+++ src/reni2/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿
 
 
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("20.1")]
-[assembly: AssemblyFileVersion("20.1.1005.2017")]
+[assembly: AssemblyFileVersion("20.1.1006.1552")]
 [assembly: AssemblyTitle("Reni Compiler: Library")]
 [assembly: AssemblyCompany("HoyerWare")]
 [assembly: AssemblyProduct("Reni Compiler")]
