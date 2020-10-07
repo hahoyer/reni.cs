3c22936d0613f5c0af7c3cebce07a6dd29a9819a
hahoyer.Anja <hahoyer@gmail.com>
2020-10-07 04:32:52 +0200

    change: rework of ValueSyntax

--- src/reni2/TokenClasses/SyntaxFactory.cs
+++ src/reni2/TokenClasses/SyntaxFactory.cs
@@ -20,9 +20,7 @@ namespace Reni.TokenClasses
                 );
                 Tracer.Assert(target.Left.Right != null);
 
-                var localFactory =
-                    new SyntaxFactory((target.TokenClass as IDefaultScopeProvider)?.MeansPublic ?? false);
-                return localFactory
+                return factory
                     .GetSyntax(target.Left.Right)
                     .Apply(syntax => syntax.ToValueSyntax(target));
             }
@@ -109,16 +107,17 @@ namespace Reni.TokenClasses
                     .Aggregate(DeclarerSyntax.Empty, (result1, result2) => Combine(result1, result2, target));
         }
 
-        internal static readonly ISyntaxFactory List = new ListHandler();
         internal static readonly IValueSyntaxFactory Bracket = new BracketHandler();
-        internal static readonly IDeclarerSyntaxFactory DeclarationMark = new DeclarationMarkHandler();
-        internal static readonly ISyntaxFactory Colon = new ColonHandler();
         internal static readonly IValueSyntaxFactory Definable = new DefinableHandler();
-        internal static readonly IDeclarerSyntaxFactory DefinableAsDeclarer = new DefinableHandler();
         internal static readonly IValueSyntaxFactory Terminal = new TerminalHandler();
+
+        internal static readonly ISyntaxFactory List = new ListHandler();
+        internal static readonly ISyntaxFactory Colon = new ColonHandler();
+        
+        internal static readonly IDeclarerSyntaxFactory DeclarationMark = new DeclarationMarkHandler();
+        internal static readonly IDeclarerSyntaxFactory DefinableAsDeclarer = new DefinableHandler();
         internal static readonly IDeclarerSyntaxFactory Declarer = new DeclarerHandler();
         internal static readonly IDeclarerSyntaxFactory ComplexDeclarer = new ComplexDeclarerHandler();
-
 
         internal static readonly SyntaxFactory Root = new SyntaxFactory(false);
 
@@ -130,7 +129,7 @@ namespace Reni.TokenClasses
         Result<DeclarerSyntax> GetDeclarerSyntax(BinaryTree target)
         {
             if(target.TokenClass is IDeclarerSyntaxFactoryToken token)
-                return token.Provider.Get(target, this);
+                return token.Provider.Get(target, GetActualFactory(target.TokenClass));
 
             NotImplementedMethod(target, nameof(target.TokenClass), target.TokenClass);
             return default;
@@ -139,16 +138,23 @@ namespace Reni.TokenClasses
         internal Result<ValueSyntax> GetValueSyntax(BinaryTree target)
         {
             if(target.TokenClass is IValueSyntaxFactoryToken token)
-                return token.Provider.Get(target, this);
+                return token.Provider.Get(target, GetActualFactory(target.TokenClass));
 
+            
             NotImplementedMethod(target, nameof(target.TokenClass), target.TokenClass);
             return default;
         }
 
-        internal Result<Syntax> GetSyntax(BinaryTree target)
+        SyntaxFactory GetActualFactory(ITokenClass tokenClass)
+        {
+            var meansPublic = (tokenClass as IDefaultScopeProvider)?.MeansPublic ?? MeansPublic;
+            return meansPublic == MeansPublic? this : new SyntaxFactory(meansPublic);
+        }
+
+        Result<Syntax> GetSyntax(BinaryTree target)
         {
             if(target.TokenClass is ISyntaxFactoryToken token)
-                return token.Provider.Get(target, this);
+                return token.Provider.Get(target, GetActualFactory(target.TokenClass));
 
             NotImplementedMethod(target, nameof(target.TokenClass), target.TokenClass);
             return default;
