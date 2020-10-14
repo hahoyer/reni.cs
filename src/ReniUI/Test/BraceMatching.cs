84c9512437f4f4ed78a392d02fa5c95bc835a22d
hahoyer.Anja <hahoyer@gmail.com>
2020-10-14 14:29:28 +0200

    refactor: reformat

--- src/ReniUI/Test/BraceMatching.cs
+++ src/ReniUI/Test/BraceMatching.cs
@@ -1,9 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using hw.DebugFormatter;
 using hw.Helper;
 using hw.UnitTest;
+
+// ReSharper disable StringIndexOfIsCultureSpecific.1
+// ReSharper disable StringIndexOfIsCultureSpecific.2
 
 namespace ReniUI.Test
 {
@@ -13,12 +14,12 @@ namespace ReniUI.Test
         [UnitTest]
         public void MatchingBraces()
         {
-            const string Text = @"(1,3,4,6)";
+            const string text = @"(1,3,4,6)";
 
-            var compiler = CompilerBrowser.FromText(Text);
+            var compiler = CompilerBrowser.FromText(text);
 
             var open = compiler.LocatePosition(0);
-            var close = compiler.LocatePosition(Text.IndexOf(")"));
+            var close = compiler.LocatePosition(text.IndexOf(")"));
 
             var matchOpen = compiler.FindAllBelongings(open);
             var matchClose = compiler.FindAllBelongings(close);
@@ -31,12 +32,12 @@ namespace ReniUI.Test
         [UnitTest]
         public void MoreMatchingBraces()
         {
-            const string Text = @"(1,3),4,6)";
+            const string text = @"(1,3),4,6)";
 
-            var compiler = CompilerBrowser.FromText(Text);
+            var compiler = CompilerBrowser.FromText(text);
 
             var open = compiler.LocatePosition(0);
-            var close = compiler.LocatePosition(Text.IndexOf(")"));
+            var close = compiler.LocatePosition(text.IndexOf(")"));
 
             var matchOpen = compiler.FindAllBelongings(open);
             var matchClose = compiler.FindAllBelongings(close);
@@ -49,11 +50,11 @@ namespace ReniUI.Test
         [UnitTest]
         public void NotMatchingBraces()
         {
-            const string Text = @"(1,3),4,6)";
+            const string text = @"(1,3),4,6)";
 
-            var compiler = CompilerBrowser.FromText(Text);
+            var compiler = CompilerBrowser.FromText(text);
 
-            var close = compiler.LocatePosition(Text.IndexOf(")", Text.IndexOf(")") + 1));
+            var close = compiler.LocatePosition(text.IndexOf(")", text.IndexOf(")") + 1));
 
             var matchClose = compiler.FindAllBelongings(close);
 
