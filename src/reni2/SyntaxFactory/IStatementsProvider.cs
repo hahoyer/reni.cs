c1cc40cb3ff50edf60581cd4d48e107054f17d5d
hahoyer.Anja <hahoyer@gmail.com>
2020-10-23 16:41:37 +0200

    change: issue handling

--- src/reni2/SyntaxFactory/IStatementsProvider.cs
+++ src/reni2/SyntaxFactory/IStatementsProvider.cs
@@ -1,4 +1,3 @@
-using Reni.Parser;
 using Reni.SyntaxTree;
 using Reni.TokenClasses;
 
@@ -6,6 +5,6 @@ namespace Reni.SyntaxFactory
 {
     interface IStatementsProvider
     {
-        Result<IStatementSyntax[]> Get(BinaryTree target, Factory factory, FrameItemContainer brackets);
+        IStatementSyntax[] Get(BinaryTree target, Factory factory, FrameItemContainer frameItems = null);
     }
 }
