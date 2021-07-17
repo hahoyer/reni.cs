805a360abbd05327e7f9067f6fae865ac47b7dd3
hahoyer <hahoyer@gmail.com>
2021-07-17 11:57:06 +0200

    bugfix: Ensure temp path exits for compiler artifacts

--- src/Reni/Code/Generator.cs
+++ src/Reni/Code/Generator.cs
@@ -88,6 +88,7 @@ namespace ReniGeneratedCode
                 "." +
                 Thread.CurrentThread.ManagedThreadId +
                 ".reni.cs";
+            name.ToSmbFile().EnsureDirectoryOfFileExists();
             CodeToFile(name, source, true);
 
 
