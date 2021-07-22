881a08c6086f06bfbea19876faad85a996e4c238
hahoyer <hahoyer@gmail.com>
2021-07-22 17:40:33 +0200

    refactor: new namespace "Formatter"

---
+++ src/ReniUI/Test/Formatting/All.cs
@@ -0,0 +1,15 @@
+using hw.UnitTest;
+using ReniUI.Test.Formatting;
+
+namespace reniUI.Test.Formatting
+{
+    [UnitTest]
+    [BadThings]
+    [Basics]
+    [Complex]
+    [Declaration]
+    [Lists]
+    [ThenElse]
+    [WithComments]
+    public class All : DependenceProvider {}
+}
