b39ae01cd1f4ab7cfdb1e1106472ac90c846a689
hahoyer-21 <hahoyer@gmail.com>
2021-10-13 00:00:57 +0200

    change: preparation for vscode extension

---
+++ src/reniVSCode/main.js
@@ -0,0 +1,7 @@
+﻿var edge = require("edge-js");
+var dotNet5 = edge.func("DotNet5.dll");
+var ten = dotNetFramework(3,true);
+
+var dotNetFramework = edge.func("DotNetFramework.dll");
+var ten = dotNetFramework(3,true);
+
