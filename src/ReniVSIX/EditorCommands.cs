45575959e479b7e253fdcae38f6a935814e79332
hahoyer-21 <hahoyer@gmail.com>
2021-11-15 19:24:36 +0100

    change: Investigation: howto implement custom formatting for reni.

---
+++ src/ReniVSIX/EditorCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using hw.DebugFormatter;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace ReniVSIX
+{
+    [Export(typeof(IVsCommandHandlerServiceAdapter))]
+    [ContentType("reni")] // This classifier applies to all text files.
+    sealed class EditorCommands : DumpableObject, IOleCommandTarget
+    {
+        int IOleCommandTarget.Exec
+            (ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            NotImplementedFunction(pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+            return default;
+        }
+
+        int IOleCommandTarget.QueryStatus
+            (ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            NotImplementedFunction(pguidCmdGroup, cCmds, prgCmds, pCmdText);
+            return default;
+        }
+    }
+}
