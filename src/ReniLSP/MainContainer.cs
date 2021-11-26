01ff264ddddc84ddb2a2a1f80d6d555ba2753c5f
hahoyer-21 <hahoyer@gmail.com>
2021-11-26 11:40:26 +0100

    change: (desperate) LSP, but does not work

--- src/ReniLSP/MainContainer.cs
+++ src/ReniLSP/MainContainer.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using hw.DebugFormatter;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using OmniSharp.Extensions.LanguageServer.Server;
 
 namespace ReniLSP
 {
     static class MainContainer
     {
+
         public static async Task Main(string[] args)
         {
             if(args.SingleOrDefault() == "none")
@@ -16,14 +23,70 @@ namespace ReniLSP
                 return;
             }
 
+            Debugger.Launch();
+
             var server = await LanguageServer.From(options
                 => options
                     .WithInput(Console.OpenStandardInput())
                     .WithOutput(Console.OpenStandardOutput())
+                    .WithLoggerFactory(new LoggerFactory())
+                    .AddDefaultLoggingProvider()
+                    .OnInitialize(Initialize)
+                    .OnInitialized(Initialized)
                     .WithHandler<Target>()
                     .WithHandler<TokenTarget>()
             );
             await server.WaitForExit;
         }
+
+        static Task Initialized
+        (
+            ILanguageServer server, InitializeParams request, InitializeResult response
+            , CancellationToken cancellationtoken
+        )
+        {
+            Dumpable.NotImplementedFunction(server, request, response, cancellationtoken);
+            return Task.CompletedTask;
+        }
+
+        static Task Initialize(ILanguageServer server, InitializeParams request, CancellationToken cancellationtoken)
+        {
+            Dumpable.NotImplementedFunction(server, request, cancellationtoken);
+            return Task.CompletedTask;
+        }
+
+
+        internal static FilePositionTag ToFilePositionTag(this LogLevel level)
+            => level switch
+            {
+                LogLevel.Trace => FilePositionTag.Debug
+                , LogLevel.Debug => FilePositionTag.Debug
+                , _ => FilePositionTag.Output
+            };
+    }
+
+    class LoggerFactory : DumpableObject, ILoggerFactory
+    {
+        class Logger : DumpableObject, ILogger
+        {
+            readonly string CategoryName;
+            public Logger(string categoryName) => CategoryName = categoryName;
+
+            IDisposable ILogger.BeginScope<TState>(TState state) => null;
+
+            bool ILogger.IsEnabled(LogLevel logLevel) => true;
+
+            void ILogger.Log<TState>
+            (
+                LogLevel logLevel, EventId eventId, TState state, Exception? exception
+                , Func<TState, Exception?, string> formatter
+            ) => ($"{CategoryName}: [{logLevel}] {formatter(state, exception)}").Log();
+        }
+
+        void IDisposable.Dispose() { }
+
+        void ILoggerFactory.AddProvider(ILoggerProvider provider) => NotImplementedMethod(provider);
+
+        ILogger ILoggerFactory.CreateLogger(string categoryName) => new Logger(categoryName);
     }
 }
