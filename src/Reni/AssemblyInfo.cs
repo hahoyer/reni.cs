78b661f9e03b387173d934275b5bee1f7ee2eb67
hahoyer-21 <hahoyer@gmail.com>
2021-11-26 11:36:52 +0100

    admin: VSIX

--- src/Reni/AssemblyInfo.cs
+++ src/Reni/AssemblyInfo.cs
@@ -1,3 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("21.1")]
+[assembly: AssemblyFileVersion("21.1.1126.1128")]
+[assembly: AssemblyTitle("Reni Compiler: Library")]
+[assembly: AssemblyCompany("HoyerWare")]
+[assembly: AssemblyProduct("Reni Compiler")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2021")]
 [assembly: InternalsVisibleTo("ReniUI")]
-[assembly: InternalsVisibleTo("ReniGenerator")]
--- src/ReniUI/AssemblyInfo.cs
+++ src/ReniUI/AssemblyInfo.cs
@@ -1,10 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("21.1")]
-[assembly: AssemblyFileVersion("21.1.1013.1358")]
-[assembly: AssemblyTitle("Reni Compiler: Library")]
+[assembly: AssemblyFileVersion("21.1.1126.1135")]
+[assembly: AssemblyTitle("Reni Compiler: Library for user interfaces")]
 [assembly: AssemblyCompany("HoyerWare")]
 [assembly: AssemblyProduct("Reni Compiler")]
 [assembly: AssemblyCopyright("Copyright © Harald Hoyer 2021")]
-[assembly: InternalsVisibleTo("ReniUI")]
