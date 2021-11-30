91b23d218bbb307513698ecfefee8736b380a467
hahoyer-21 <hahoyer@gmail.com>
2021-11-30 23:59:03 +0100

    new: syntax highlighting VS2019

--- src/ReniVSIX/Classifier.cs
+++ src/ReniVSIX/Classifier.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using hw.DebugFormatter;
 using hw.Helper;
+using hw.Scanner;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Tagging;
 using ReniUI;
 using ReniUI.Classification;
 
 namespace ReniVSIX
 {
-    class Classifier : DumpableObject, IClassifier
+    class Classifier : DumpableObject, IClassifier, ITagger<IErrorTag>
     {
         readonly ITextBuffer Buffer;
         readonly IClassificationTypeRegistryService Registry;
@@ -26,38 +28,61 @@ namespace ReniVSIX
         public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
 
         IList<ClassificationSpan> IClassifier.GetClassificationSpans(SnapshotSpan span)
+            => CompilerBrowser
+                .FromText(Buffer.CurrentSnapshot.GetText())
+                .GetClassification(span.Start.Position, span.End.Position).ToArray()
+                .Select(item => GetClassificationSpan(item, span)).ToArray()
+                .ToList();
+
+        IEnumerable<ITagSpan<IErrorTag>> ITagger<IErrorTag>.GetTags(NormalizedSnapshotSpanCollection spans) 
+            => spans
+            .Select(GetTag)
+            .ToArray();
+
+        ITagSpan<IErrorTag> GetTag(SnapshotSpan span)
         {
-            var items = CompilerBrowser.FromText(Buffer.CurrentSnapshot.GetText())
-                .GetClassification(span.Start.Position, span.End.Position).ToArray();
-            var spans = items
-                .Select(item => CreateClassificationSpan(item, span)).ToArray();
-            return spans.ToList();
+            var item = CompilerBrowser
+                .FromText(Buffer.CurrentSnapshot.GetText()).Locate(span.Start.Position);
+
+            
+            var snapshotSpan = new TagSpan<IErrorTag>(span,new 
+                ErrorTag("syntax error", item));
+            
+            
+            throw new NotImplementedException();
         }
+
+        SourcePart GetSourcePart(SnapshotSpan span) 
+            => (new Source(Buffer.CurrentSnapshot.GetText()) + span.Start.Position).Span(span.Length);
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
 
         IClassificationType GetClassificationType(string s) => Registry.GetClassificationType(s);
 
         IClassificationType GetFormatTypeName(Item item)
         {
-            if(item.IsComment)
-                return Types["comment"];
-            if(item.IsLineComment)
-                return Types["comment"];
-            if(item.IsWhiteSpace)
-                return Types["text"];
-            if(item.IsLineEnd)
-                return Types["text"];
             if(item.IsNumber)
                 return Types["number"];
-            if(item.IsText)
-                return Types["string"];
             if(item.IsKeyword)
                 return Types["keyword"];
             if(item.IsIdentifier)
                 return Types["identifier"];
+            if(item.IsWhiteSpace)
+                return Types["text"];
+            if(item.IsLineEnd)
+                return Types["text"];
+            if(item.IsError)
+                return Types["syntax error"];
+            if(item.IsText)
+                return Types["string"];
+            if(item.IsComment)
+                return Types["comment"];
+            if(item.IsLineComment)
+                return Types["comment"];
             return Types["text"];
         }
 
-        ClassificationSpan CreateClassificationSpan(Item item, SnapshotSpan all)
+        ClassificationSpan GetClassificationSpan(Item item, SnapshotSpan all)
         {
             var sourcePart = item.SourcePart;
             var snapshotSpan = new SnapshotSpan(all.Snapshot, new Span(sourcePart.Position, sourcePart.Length));
@@ -66,7 +91,4 @@ namespace ReniVSIX
             return new ClassificationSpan(snapshotSpan, formatTypeName);
         }
     }
-
-
-    static class Extension { }
 }
