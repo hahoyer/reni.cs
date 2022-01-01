5bb8ed0df5a9233e3c19560988db8dbba01b4a7c
hahoyer-21 <hahoyer@gmail.com>
2022-01-01 21:05:27 +0100

    change: Dumper for MS-library classes

---
+++ src/ReniVSIX/Dumper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Reflection;
+using hw.DebugFormatter;
+using Microsoft.VisualStudio.Package;
+
+namespace ReniVSIX
+{
+    static class Dumper
+    {
+        static bool RequestMethods(MemberInfo memberInfo, ParseRequest target)
+        {
+            switch(memberInfo.Name)
+            {
+                case nameof(target.Callback):
+                case nameof(target.Text):
+                case nameof(target.View):
+                    return false;
+            }
+
+            var field = memberInfo as FieldInfo;
+            if(field != null)
+                return field.IsPublic;
+
+            var property = memberInfo as PropertyInfo;
+            if(property != null)
+                return property.CanRead && property.GetMethod.IsPublic;
+
+            Tracer.TraceBreak();
+            return default;
+        }
+
+        static string Dump(Type type, ParseRequest target)
+        {
+            Tracer.TraceBreak();
+            return default;
+        }
+
+        public static void Register()
+            => Tracer.Dumper.Configuration.Handlers.Add<ParseRequest>(methodCheck: RequestMethods);
+    }
+}
