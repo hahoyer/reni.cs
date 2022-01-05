b60e0f0aff7f6649f06a658f9fea47c4293dfb03
hahoyer-21 <hahoyer@gmail.com>
2022-01-05 15:41:16 +0100

    bugfix: CompoundSyntax with CleanupHandler: List anchors are missing

--- src/Reni/SyntaxFactory/CleanupHandler.cs
+++ src/Reni/SyntaxFactory/CleanupHandler.cs
@@ -2,17 +2,16 @@ using hw.DebugFormatter;
 using Reni.SyntaxTree;
 using Reni.TokenClasses;
 
-namespace Reni.SyntaxFactory
+namespace Reni.SyntaxFactory;
+
+sealed class CleanupHandler : DumpableObject, IValueProvider
 {
-    sealed class CleanupHandler : DumpableObject, IValueProvider
+    ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor frameAnchor)
     {
-        ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor frameAnchor)
-        {
-            var statements = factory.GetStatementsSyntax(target.Left, null, target.Left?.TokenClass);
-            var cleanup = factory.GetValueSyntax(target.Right);
-            var cleanupSection = new CleanupSyntax(cleanup, Anchor.Create(target));
-            var anchor = Anchor.Create(target.Left?.ParserLevelGroup).Combine(frameAnchor);
-            return CompoundSyntax.Create(statements, cleanupSection, anchor);
-        }
+        var statements = factory.GetStatementsSyntax(target.Left, null, target.Left?.TokenClass);
+        var cleanup = factory.GetValueSyntax(target.Right);
+        var cleanupSection = new CleanupSyntax(cleanup, Anchor.Create(target));
+        var anchor = frameAnchor + Anchor.CheckedCreate(target.Left?.ParserLevelGroup);
+        return CompoundSyntax.Create(statements, cleanupSection, anchor);
     }
 }
--- src/Reni/SyntaxTree/Anchor.cs
+++ src/Reni/SyntaxTree/Anchor.cs
@@ -9,82 +9,91 @@ using Reni.Parser;
 using Reni.TokenClasses;
 using Reni.Validation;
 
-namespace Reni.SyntaxTree
+namespace Reni.SyntaxTree;
+
+public sealed class Anchor : DumpableObject, ValueCache.IContainer
 {
-    public sealed class Anchor : DumpableObject, ValueCache.IContainer
+    public readonly BinaryTree[] Items;
+
+    Anchor(params BinaryTree[] items)
     {
-        public readonly BinaryTree[] Items;
+        Items = items
+            .Where(item => item != null)
+            .Distinct()
+            .OrderBy(item => item.Token.Position)
+            .ToArray();
 
-        Anchor(params BinaryTree[] items)
-        {
-            Items = items
-                .Where(item => item != null)
-                .Distinct()
-                .OrderBy(item => item.Token.Position)
-                .ToArray();
-
-            Items.Any().Assert();
-            Main.AssertIsNotNull();
-        }
-
-        ValueCache ValueCache.IContainer.Cache { get; } = new();
-
-        protected override string GetNodeDump() => base.GetNodeDump() + $"[{Items.Length}]";
-
-        [DisableDump]
-        internal SourcePart[] SourceParts => Items.SourceParts();
-
-        [DisableDump]
-        internal SourcePart SourcePart => SourceParts.Combine();
-
-        [DisableDump]
-        public IEnumerable<Issue> Issues => Items.SelectMany(node => node.AllIssues);
-
-        [DisableDump]
-        public bool IsEmpty => !Items.Any();
-
-        [DisableDump]
-        internal BinaryTree Main => this.CachedValue(GetMain);
-
-        internal Anchor GetLeftOf(BinaryTree target) => GetLeftOf(target.Token.Start);
-        internal Anchor GetRightOf(BinaryTree target) => GetRightOf(target.Token.End);
-
-        [PublicAPI]
-        internal Anchor GetLeftOf(SourcePosition position)
-            => new(Items.Where(item => item.Token < position).ToArray());
-
-        [PublicAPI]
-        internal Anchor GetRightOf(SourcePosition position)
-            => new(Items.Where(item => position < item.Token).ToArray());
-
-        internal static Anchor Create(BinaryTree leftAnchor, BinaryTree rightAnchor)
-            => new(leftAnchor, rightAnchor);
-
-        internal static Anchor Create(BinaryTree leftAnchor)
-            => new(leftAnchor.AssertNotNull());
-
-        internal static Anchor Create(params BinaryTree[] items) => new(items);
-
-        internal Anchor Combine(Anchor other) => Combine(other?.Items);
-
-        internal Anchor Combine(BinaryTree[] other)
-        {
-            if(other == null || !other.Any())
-                return this;
-            return new(other.Concat(Items).ToArray());
-        }
-
-        BinaryTree GetMain()
-            => Items
-                .Single(node => Items.All(parent => !node.HasAsParent(parent)));
-
-        public void SetSyntax(Syntax syntax)
-        {
-            foreach(var item in Items)
-                item.SetSyntax(syntax);
-        }
-
-        public static Anchor CreateAll(BinaryTree target)
-            => Create(target.GetNodesFromLeftToRight().ToArray());
+        Items.Any().Assert();
+        Main.AssertIsNotNull();
     }
+
+    ValueCache ValueCache.IContainer.Cache { get; } = new();
+
+    protected override string GetNodeDump() => base.GetNodeDump() + $"[{Items.Length}]";
+
+    [DisableDump]
+    internal SourcePart[] SourceParts => Items.SourceParts();
+
+    [DisableDump]
+    internal SourcePart SourcePart => SourceParts.Combine();
+
+    [DisableDump]
+    public IEnumerable<Issue> Issues => Items.SelectMany(node => node.AllIssues);
+
+    [DisableDump]
+    public bool IsEmpty => !Items.Any();
+
+    [DisableDump]
+    internal BinaryTree Main => this.CachedValue(GetMain);
+
+    internal Anchor GetLeftOf(BinaryTree target) => GetLeftOf(target.Token.Start);
+    internal Anchor GetRightOf(BinaryTree target) => GetRightOf(target.Token.End);
+
+    [PublicAPI]
+    internal Anchor GetLeftOf(SourcePosition position)
+        => new(Items.Where(item => item.Token < position).ToArray());
+
+    [PublicAPI]
+    internal Anchor GetRightOf(SourcePosition position)
+        => new(Items.Where(item => position < item.Token).ToArray());
+
+    internal static Anchor Create(BinaryTree leftAnchor, BinaryTree rightAnchor)
+        => new(leftAnchor, rightAnchor);
+
+    internal static Anchor Create(BinaryTree leftAnchor)
+        => new(leftAnchor.AssertNotNull());
+
+    internal static Anchor Create(params BinaryTree[] items) => new(items);
+
+    internal static Anchor CheckedCreate(params BinaryTree[] items) 
+        => items == null || items.Length == 0? null : new(items);
+
+    internal Anchor Combine(Anchor other) => Combine(other?.Items);
+
+    internal Anchor Combine(BinaryTree[] other)
+    {
+        if(other == null || !other.Any())
+            return this;
+        return new(other.Concat(Items).ToArray());
+    }
+
+    BinaryTree GetMain()
+        => Items
+            .Single(node => Items.All(parent => !node.HasAsParent(parent)));
+
+    public void SetSyntax(Syntax syntax)
+    {
+        foreach(var item in Items)
+            item.SetSyntax(syntax);
+    }
+
+    public static Anchor CreateAll(BinaryTree target)
+        => Create(target.GetNodesFromLeftToRight().ToArray());
+
+    public static Anchor operator +(Anchor left, Anchor right)
+        => left == null
+            ? right
+            : right == null
+                ? left
+                : left.Combine(right);
 }
