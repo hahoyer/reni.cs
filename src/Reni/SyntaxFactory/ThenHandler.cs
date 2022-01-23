a68addc81a15cdbc739c31880722cb56a7149aca
hahoyer-21 <hahoyer@gmail.com>
2022-01-23 23:17:58 +0100

    refactor: upgrade C#

--- src/Reni/SyntaxFactory/ThenHandler.cs
+++ src/Reni/SyntaxFactory/ThenHandler.cs
@@ -2,17 +2,16 @@ using hw.DebugFormatter;
 using Reni.SyntaxTree;
 using Reni.TokenClasses;
 
-namespace Reni.SyntaxFactory
+namespace Reni.SyntaxFactory;
+
+sealed class ThenHandler : DumpableObject, IValueProvider
 {
-    class ThenHandler : DumpableObject, IValueProvider
-    {
-        ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor anchor)
-            => new CondSyntax
-            (
-                factory.GetValueSyntax(target.Left)
-                , factory.GetValueSyntax(target.Right)
-                , null
-                , Anchor.Create(target).Combine(anchor)
-            );
-    }
+    ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor anchor)
+        => new CondSyntax
+        (
+            factory.GetValueSyntax(target.Left)
+            , factory.GetValueSyntax(target.Right)
+            , null
+            , Anchor.Create(target).Combine(anchor)
+        );
 }
