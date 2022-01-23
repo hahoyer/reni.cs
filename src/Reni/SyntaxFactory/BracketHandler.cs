a16b304b79803dbb670a26791516b13ade09eb64
hahoyer-21 <hahoyer@gmail.com>
2022-01-23 23:10:45 +0100

    refactor: upgrade C#

--- src/Reni/SyntaxFactory/BracketHandler.cs
+++ src/Reni/SyntaxFactory/BracketHandler.cs
@@ -2,16 +2,15 @@ using hw.DebugFormatter;
 using Reni.SyntaxTree;
 using Reni.TokenClasses;
 
-namespace Reni.SyntaxFactory
+namespace Reni.SyntaxFactory;
+
+sealed class BracketHandler : DumpableObject, IValueProvider
 {
-    class BracketHandler : DumpableObject, IValueProvider
+    ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor anchor)
     {
-        ValueSyntax IValueProvider.Get(BinaryTree target, Factory factory, Anchor anchor)
-        {
-            var kernel = target.BracketKernel;
-            var result = factory.GetValueSyntax(kernel.Center, kernel.ToAnchor.Combine(anchor));
-            result.AssertIsNotNull();
-            return result;
-        }
+        var kernel = target.BracketKernel;
+        var result = factory.GetValueSyntax(kernel.Center, kernel.ToAnchor.Combine(anchor));
+        result.AssertIsNotNull();
+        return result;
     }
 }
