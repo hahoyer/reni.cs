98632fc8153c99de14883c7a48ef0366702b4acd
hahoyer-21 <hahoyer@gmail.com>
2022-01-24 20:55:39 +0100

    refactor: upgrade C#

--- src/ReniUI/Test/Formatting/CommentsOfLists.cs
+++ src/ReniUI/Test/Formatting/CommentsOfLists.cs
@@ -1,74 +1,71 @@
 using hw.UnitTest;
 
-namespace ReniUI.Test.Formatting
+namespace ReniUI.Test.Formatting;
+
+[UnitTest]
+[Lists]
+public class CommentsOfLists : DependenceProvider
 {
     [UnitTest]
-    [Lists]
-    public class CommentsOfLists : DependenceProvider
+    public void ComplexText1()
     {
-        [UnitTest]
-        public void ComplexText1()
-        {
-            const string text =
-                @"#( asdf )# head
+        const string text =
+            @"#( asdf )# head
 #( asdf)# comment )# waggon
 ";
-            const string expectedText =
-                @"#( asdf )# head
+        const string expectedText =
+            @"#( asdf )# head
     #( asdf)# comment )# waggon
 ";
-            text.SimpleFormattingTest(expectedText);
-        }
-
-        [UnitTest]
-        [Test]
-        public void ComplexText()
-        {
-            const string text =
-                @"#( asdf )#  texxxxxxxxxxxt
-
-texxxxxxx
-
-#(
-    comment
-)#
-texxxxxxx
-
-#( asdf)# comment )#  texxxxxxxxxxxt
-
-#(asdf comment )# comment asd)# comment asdf)# asdf)# texxxxxxxxxxxt
-
-#(asdf comment #( comment )# comment asd)# comment asdf)# texxxxxxxxxxxt
-
-#( )# texxxxxxxxxxxt
-
-#()# texxxxxxxxxxxt";
-
-
-            const string expectedText =
-                @"#( asdf )# texxxxxxxxxxxt
-
-    texxxxxxx
-
-#(
-    comment
-)#
-    texxxxxxx
-
-#( asdf)# comment )#  texxxxxxxxxxxt
-
-#(asdf comment )# comment asd)# comment asdf)# asdf)# texxxxxxxxxxxt
-
-#(asdf comment #( comment )# comment asd)# comment asdf)# texxxxxxxxxxxt
-
-#( )# texxxxxxxxxxxt
-
-#()# texxxxxxxxxxxt";
-
-
-            text.SimpleFormattingTest(expectedText);
-        }
-
+        text.SimpleFormattingTest(expectedText);
     }
 
+    [UnitTest]
+    [Test]
+    public void ComplexText()
+    {
+        const string text =
+            @"#( asdf )#  texxxxxxxxxxxt
+
+texxxxxxx
+
+#(
+    comment
+)#
+texxxxxxx
+
+#( asdf)# comment )#  texxxxxxxxxxxt
+
+#(asdf comment )# comment asd)# comment asdf)# asdf)# texxxxxxxxxxxt
+
+#(asdf comment #( comment )# comment asd)# comment asdf)# texxxxxxxxxxxt
+
+#( )# texxxxxxxxxxxt
+
+#()# texxxxxxxxxxxt";
+
+
+        const string expectedText =
+            @"#( asdf )# texxxxxxxxxxxt
+
+    texxxxxxx
+
+#(
+    comment
+)#
+    texxxxxxx
+
+#( asdf)# comment )#  texxxxxxxxxxxt
+
+#(asdf comment )# comment asd)# comment asdf)# asdf)# texxxxxxxxxxxt
+
+#(asdf comment #( comment )# comment asd)# comment asdf)# texxxxxxxxxxxt
+
+#( )# texxxxxxxxxxxt
+
+#()# texxxxxxxxxxxt";
+
+
+        text.SimpleFormattingTest(expectedText);
+    }
 }
