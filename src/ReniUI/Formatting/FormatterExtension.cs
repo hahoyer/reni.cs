3a632353ce8f959310378535261948b9d3db4920
hahoyer-21 <hahoyer@gmail.com>
2022-01-28 21:16:24 +0100

    bugfix: formatting: IsTooSmall function crashed on empty source

--- src/ReniUI/Formatting/FormatterExtension.cs
+++ src/ReniUI/Formatting/FormatterExtension.cs
@@ -3,65 +3,66 @@ using System.Linq;
 using hw.DebugFormatter;
 using hw.Scanner;
 
-namespace ReniUI.Formatting
+namespace ReniUI.Formatting;
+
+static class FormatterExtension
 {
-    static class FormatterExtension
+    public static IFormatter Create(this Configuration configuration)
+        => new FormatterByBinaryTree(configuration ?? new Configuration());
+
+    internal static string Combine(this IEnumerable<Edit> pieces, SourcePart targetPart)
     {
-        public static IFormatter Create(this Configuration configuration)
-            => new FormatterByBinaryTree(configuration ?? new Configuration());
+        pieces = pieces.ToArray();
+        var original = targetPart.Id;
+        var originalPosition = targetPart.Position;
+        var originalEndPosition = originalPosition + original.Length;
 
-        internal static string Combine(this IEnumerable<Edit> pieces, SourcePart targetPart)
+        var currentPosition = -originalPosition;
+        var result = "";
+
+        var edits = pieces
+            .OrderBy(edit => edit.Remove.Position)
+            .Where(edit => edit.Remove.Intersect(targetPart) != null)
+            .ToArray();
+
+        foreach(var edit in edits)
         {
-            pieces = pieces.ToArray();
-            var original = targetPart.Id;
-            var originalPosition = targetPart.Position;
-            var originalEndPosition = originalPosition + original.Length;
-
-            var currentPosition = -originalPosition;
-            var result = "";
-
-            var edits = pieces
-                .OrderBy(edit => edit.Remove.Position)
-                .Where(edit => edit.Remove.Intersect(targetPart) != null)
-                .ToArray();
-
-            foreach(var edit in edits)
+            (edit.Remove.EndPosition <= originalEndPosition).Assert("not implemented.");
+            var newPosition = edit.Remove.EndPosition - originalPosition;
+            if(currentPosition < 0)
+                (newPosition <= 0).Assert();
+            else
             {
-                (edit.Remove.EndPosition <= originalEndPosition).Assert("not implemented.");
-                var newPosition = edit.Remove.EndPosition - originalPosition;
-                if(currentPosition < 0)
-                    (newPosition <= 0).Assert();
-                else
-                {
-                    var length = edit.Remove.Position - originalPosition - currentPosition;
-                    var itemResult = original.Substring(currentPosition, length) + edit.Insert;
-                    result += itemResult;
-                }
-
-                currentPosition = newPosition;
+                var length = edit.Remove.Position - originalPosition - currentPosition;
+                var itemResult = original.Substring(currentPosition, length) + edit.Insert;
+                result += itemResult;
             }
 
-            result += original.Substring(T(0, currentPosition).Max());
-            return result;
+            currentPosition = newPosition;
         }
 
-        public static bool IsTooSmall(this CompilerBrowser compiler, SourcePart targetPart)
-        {
-            if(targetPart == null)
-                return false;
-
-            var start = compiler.Locate(targetPart.Start);
-            var end = compiler.Locate(targetPart.End+- 1);
-            if(start != null && end != null)
-                return start.Anchor == end.Anchor && IsTooSmall(start.SourcePart, targetPart);
-
-            Dumpable.NotImplementedFunction(compiler, targetPart);
-            return default;
-        }
-
-        static bool IsTooSmall(SourcePart fullToken, SourcePart targetPart)
-            => fullToken.Contains(targetPart);
-
-        public static TValue[] T<TValue>(params TValue[] value) => value;
+        result += original.Substring(T(0, currentPosition).Max());
+        return result;
     }
+
+    public static bool IsTooSmall(this CompilerBrowser compiler, SourcePart targetPart)
+    {
+        if(targetPart == null)
+            return false;
+        if(targetPart.Length == 0)
+            return true;
+
+        var start = compiler.Locate(targetPart.Start);
+        var end = compiler.Locate(targetPart.End + -1);
+        if(start != null && end != null)
+            return start.Anchor == end.Anchor && IsTooSmall(start.SourcePart, targetPart);
+
+        Dumpable.NotImplementedFunction(compiler, targetPart);
+        return default;
+    }
+
+    static bool IsTooSmall(SourcePart fullToken, SourcePart targetPart)
+        => fullToken.Contains(targetPart);
+
+    static TValue[] T<TValue>(params TValue[] value) => value;
 }
