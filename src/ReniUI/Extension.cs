2d2e619b1cc7bb7fcc8b2894d0ffc219a94bd737
hh <hahoyer@gmail.com>
2022-07-20 22:11:54 +0200

    refactor: upgrade C# refactor: reformat

--- src/ReniUI/Extension.cs
+++ src/ReniUI/Extension.cs
@@ -6,75 +6,73 @@ using hw.DebugFormatter;
 using hw.Scanner;
 using Reni.TokenClasses.Whitespace;
 
-namespace ReniUI
+namespace ReniUI;
+
+static class Extension
 {
-    static class Extension
+    sealed class QueryClass<T> : IEnumerable<T>
     {
-        sealed class QueryClass<T> : IEnumerable<T>
-        {
-            readonly Func<IEnumerable<T>> Function;
-            public QueryClass(Func<IEnumerable<T>> function) => Function = function;
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-            IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
-            IEnumerator<T> GetEnumerator() => Function().GetEnumerator();
-        }
-
-        internal static string FilePosition(this SourcePart sourcePart)
-        {
-            var source = sourcePart.Source;
-            var position = sourcePart.Position;
-            var positionEnd = sourcePart.EndPosition;
-            return source.Identifier +
-                "(" +
-                (source.LineIndex(position) + 1) +
-                "," +
-                (source.ColumnIndex(position) + 1) +
-                "," +
-                (source.LineIndex(positionEnd) + 1) +
-                "," +
-                (source.ColumnIndex(positionEnd) + 1) +
-                ")";
-        }
-
-        internal static IEnumerable<T> Query<T>(Func<IEnumerable<T>> function)
-            => new QueryClass<T>(function);
-
-        internal static SourcePart Combine(this IEnumerable<SourcePart> target)
-            => hw.Scanner.SourcePart.SaveCombine(target).Single();
-
-        internal static IItem LocateToken(this SourcePart prefix, SourcePosition offset)
-        {
-            if(prefix.Length == 0)
-                return null;
-            Dumpable.NotImplementedFunction(prefix, offset);
-            return default;
-        }
-
-        internal static bool HasStableLineBreak(this SourcePart prefix)
-        {
-            Dumpable.NotImplementedFunction(prefix);
-            return default;
-        }
-
-        [Obsolete("",true)]
-        internal static bool HasLines(this IEnumerable<IItem> whiteSpaces)
-            => whiteSpaces?.Any(HasLines) ?? false;
-
-        [Obsolete("",true)]
-        internal static bool HasLines(this IItem item) 
-            => item.ScannerTokenType is ILineBreak;
-
-        internal static SourcePart SourcePart(this IItem item)
-        {
-            Dumpable.NotImplementedFunction(item);
-            return default;
-        }
-
-        internal static SourcePart SourcePart(this IEnumerable<IItem> items)
-        {
-            Dumpable.NotImplementedFunction(items);
-            return default;
-        }
+        readonly Func<IEnumerable<T>> Function;
+        public QueryClass(Func<IEnumerable<T>> function) => Function = function;
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
+        IEnumerator<T> GetEnumerator() => Function().GetEnumerator();
     }
 
+    internal static string FilePosition(this SourcePart sourcePart)
+    {
+        var source = sourcePart.Source;
+        var position = sourcePart.Position;
+        var positionEnd = sourcePart.EndPosition;
+        return source.Identifier +
+            "(" +
+            (source.LineIndex(position) + 1) +
+            "," +
+            (source.ColumnIndex(position) + 1) +
+            "," +
+            (source.LineIndex(positionEnd) + 1) +
+            "," +
+            (source.ColumnIndex(positionEnd) + 1) +
+            ")";
+    }
+
+    internal static IEnumerable<T> Query<T>(Func<IEnumerable<T>> function)
+        => new QueryClass<T>(function);
+
+    internal static SourcePart Combine(this IEnumerable<SourcePart> target)
+        => hw.Scanner.SourcePart.SaveCombine(target).Single();
+
+    internal static IItem LocateToken(this SourcePart prefix, SourcePosition offset)
+    {
+        if(prefix.Length == 0)
+            return null;
+        Dumpable.NotImplementedFunction(prefix, offset);
+        return default;
+    }
+
+    internal static bool HasStableLineBreak(this SourcePart prefix)
+    {
+        Dumpable.NotImplementedFunction(prefix);
+        return default;
+    }
+
+    [Obsolete("", true)]
+    internal static bool HasLines(this IEnumerable<IItem> whiteSpaces)
+        => whiteSpaces?.Any(HasLines) ?? false;
+
+    [Obsolete("", true)]
+    internal static bool HasLines(this IItem item)
+        => item.ScannerTokenType is ILineBreak;
+
+    internal static SourcePart SourcePart(this IItem item)
+    {
+        Dumpable.NotImplementedFunction(item);
+        return default;
+    }
+
+    internal static SourcePart SourcePart(this IEnumerable<IItem> items)
+    {
+        Dumpable.NotImplementedFunction(items);
+        return default;
+    }
 }
