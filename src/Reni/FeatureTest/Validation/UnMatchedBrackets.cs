815c733103be898448dadca0e040cb00ce968af1
Harald Herbert Hoyer <hahoyer@gmail.com>
2022-11-21 03:15:22 +0100

    change: declarations

--- src/Reni/FeatureTest/Validation/UnMatchedBrackets.cs
+++ src/Reni/FeatureTest/Validation/UnMatchedBrackets.cs
@@ -5,51 +5,49 @@ using hw.UnitTest;
 using Reni.FeatureTest.Helper;
 using Reni.Validation;
 
-namespace Reni.FeatureTest.Validation
+namespace Reni.FeatureTest.Validation;
+
+[UnitTest]
+[Target(@"x:{ 1 x ( }; 1 dump_print")]
+public sealed class UnMatchedLeftParenthesis : CompilerTest
 {
-    [UnitTest]
-    [Target(@"x:{ 1 x ( }; 1 dump_print")]
-    public sealed class UnMatchedLeftParenthesis : CompilerTest
+    public UnMatchedLeftParenthesis()
     {
-        public UnMatchedLeftParenthesis()
-        {
-            Parameters.TraceOptions.Parser = false;
-            Parameters.CompilationLevel = CompilationLevel.Syntax;
-        }
-
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            var issueBase = issueArray[i];
-            (issueBase.IssueId == IssueId.MissingRightBracket).Assert(issueBase.Dump);
-            (issueBase.Position.GetDumpAroundCurrent(2) == "... ([] }...").Assert(issueBase.Dump);
-            i++;
-            (i == issueArray.Length).Assert(()=>Tracer.Dump(issueArray));
-        }
+        Parameters.TraceOptions.Parser = false;
+        Parameters.CompilationLevel = CompilationLevel.Syntax;
     }
 
-    [UnitTest]
-    [Target(@"x:{ 1 x ) }; 1 dump_print")]
-    public sealed class UnMatchedRightParenthesis : CompilerTest
+    protected override void Verify(IEnumerable<Issue> issues)
     {
-        public UnMatchedRightParenthesis() 
-            => Parameters.CompilationLevel = CompilationLevel.Syntax;
-
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            var issueBase = issueArray[i];
-            (issueBase.IssueId == IssueId.MissingLeftBracket).Assert(issueBase.Dump);
-            i++;
-            (i == issueArray.Length).Assert();
-        }
+        var issueArray = issues.ToArray();
+        var i = 0;
+        var issueBase = issueArray[i];
+        (issueBase.IssueId == IssueId.MissingRightBracket).Assert(issueBase.Dump);
+        (issueBase.Position.GetDumpAroundCurrent(2) == "... ([] }...").Assert(issueBase.Dump);
+        i++;
+        (i == issueArray.Length).Assert(() => Tracer.Dump(issueArray));
     }
-
-
-    [UnitTest]
-    [UnMatchedLeftParenthesis]
-    [UnMatchedRightParenthesis]
-    public sealed class UnMatchedBrackets : CompilerTest {}
 }
+
+[UnitTest]
+[Target(@"x:{ 1 x ) }; 1 dump_print")]
+public sealed class UnMatchedRightParenthesis : CompilerTest
+{
+    public UnMatchedRightParenthesis()
+        => Parameters.CompilationLevel = CompilationLevel.Syntax;
+
+    protected override void Verify(IEnumerable<Issue> issues)
+    {
+        var issueArray = issues.ToArray();
+        var i = 0;
+        var issueBase = issueArray[i];
+        (issueBase.IssueId == IssueId.MissingLeftBracket).Assert(issueBase.Dump);
+        i++;
+        (i == issueArray.Length).Assert();
+    }
+}
+
+[UnitTest]
+[UnMatchedLeftParenthesis]
+[UnMatchedRightParenthesis]
+public sealed class UnMatchedBrackets : CompilerTest { }
