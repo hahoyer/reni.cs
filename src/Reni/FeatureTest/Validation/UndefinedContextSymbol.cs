1f35da7c02235bb6e62bf4701250db065c16b881
Harald Herbert Hoyer <hahoyer@gmail.com>
2022-12-31 15:28:57 +0100

    test

--- src/Reni/FeatureTest/Validation/UndefinedContextSymbol.cs
+++ src/Reni/FeatureTest/Validation/UndefinedContextSymbol.cs
@@ -5,75 +5,76 @@ using hw.UnitTest;
 using Reni.FeatureTest.Helper;
 using Reni.Validation;
 
-namespace Reni.FeatureTest.Validation
+namespace Reni.FeatureTest.Validation;
+
+[UnitTest]
+[Target(@"x")]
+[Output("")]
+public sealed class UndefinedContextSymbol : CompilerTest
 {
-    [UnitTest]
-    [Target(@"x")]
-    [Output("")]
-    public sealed class UndefinedContextSymbol : CompilerTest
-    {
-        public UndefinedContextSymbol() => Parameters.ProcessErrors = true;
+    public UndefinedContextSymbol() => Parameters.ProcessErrors = true;
 
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
-            i++;
-            (i == issueArray.Length).Assert();
-        }
+    protected override void Verify(IEnumerable<Issue> issues)
+    {
+        var issueArray = issues.ToArray();
+        var i = 0;
+        (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
+        i++;
+        (i == issueArray.Length).Assert();
     }
+}
 
-    [UnitTest]
-    [Target(@"x: 3; x x")]
-    [Output("")]
-    public sealed class UndefinedSymbol : CompilerTest
+[UnitTest]
+[Target(@"x: 3; x x")]
+[Output("")]
+public sealed class UndefinedSymbol : CompilerTest
+{
+    public UndefinedSymbol() => Parameters.ProcessErrors = true;
+
+    protected override void Verify(IEnumerable<Issue> issues)
     {
-        public UndefinedSymbol() => Parameters.ProcessErrors = true;
-
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            (issueArray[i].IssueId == IssueId.MissingDeclarationForType).Assert(issueArray[i].Dump);
-            i++;
-            (i == issueArray.Length).Assert();
-        }
+        var issueArray = issues.ToArray();
+        var i = 0;
+        (issueArray[i].IssueId == IssueId.MissingDeclarationForType).Assert(issueArray[i].Dump);
+        i++;
+        (i == issueArray.Length).Assert();
     }
+}
 
-    [UnitTest]
-    [Target(@"x dump_print")]
-    [Output("")]
-    [UndefinedContextSymbol]
-    public sealed class UseOfUndefinedContextSymbol : CompilerTest
+[UnitTest]
+[Target(@"x dump_print")]
+[Output("")]
+[UndefinedContextSymbol]
+public sealed class UseOfUndefinedContextSymbol : CompilerTest
+{
+    public UseOfUndefinedContextSymbol() => Parameters.ProcessErrors = true;
+
+    protected override void Verify(IEnumerable<Issue> issues)
     {
-        public UseOfUndefinedContextSymbol() => Parameters.ProcessErrors = true;
-
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
-            i++;
-            (i == issueArray.Length).Assert();
-        }
+        var issueArray = issues.ToArray();
+        var i = 0;
+        (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
+        i++;
+        (issueArray[i].IssueId == IssueId.MissingDeclarationForType).Assert(issueArray[i].Dump);
+        i++;
+        (i == issueArray.Length).Assert();
     }
+}
 
-    [UnitTest]
-    [Target(@"x x dump_print")]
-    [Output("")]
-    [UseOfUndefinedContextSymbol]
-    public sealed class IndirectUseOfUndefinedContextSymbol : CompilerTest
+[UnitTest]
+[Target(@"x x dump_print")]
+[Output("")]
+[UseOfUndefinedContextSymbol]
+public sealed class IndirectUseOfUndefinedContextSymbol : CompilerTest
+{
+    public IndirectUseOfUndefinedContextSymbol() => Parameters.ProcessErrors = true;
+
+    protected override void Verify(IEnumerable<Issue> issues)
     {
-        public IndirectUseOfUndefinedContextSymbol() => Parameters.ProcessErrors = true;
-
-        protected override void Verify(IEnumerable<Issue> issues)
-        {
-            var issueArray = issues.ToArray();
-            var i = 0;
-            (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
-            i++;
-            (i == issueArray.Length).Assert();
-        }
+        var issueArray = issues.ToArray();
+        var i = 0;
+        (issueArray[i].IssueId == IssueId.MissingDeclarationInContext).Assert(issueArray[i].Dump);
+        i++;
+        (i == issueArray.Length).Assert();
     }
 }
