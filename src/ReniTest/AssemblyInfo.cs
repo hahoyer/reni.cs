e7c41d7554d7de43ba482a31e8dd408f5a8bdbb8
Harald Herbert Hoyer <hahoyer@gmail.com>
2023-01-09 22:52:33 +0100

    admin

--- src/ReniTest/AssemblyInfo.cs
+++ src/ReniTest/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("22.1")]
-[assembly: AssemblyFileVersion("22.1.1208.2102")]
+[assembly: AssemblyVersion("23.1")]
+[assembly: AssemblyFileVersion("23.1.0109.2248")]
 [assembly: AssemblyTitle("Reni Compiler: Library")]
 [assembly: AssemblyCompany("HoyerWare")]
 [assembly: AssemblyProduct("Reni Compiler")]
-[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2022")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2023")]
 [assembly: InternalsVisibleTo("ReniUI")]
--- src/ReniUI/AssemblyInfo.cs
+++ src/ReniUI/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("22.0")]
-[assembly: AssemblyFileVersion("22.0.1217.0044")]
+[assembly: AssemblyVersion("23.0")]
+[assembly: AssemblyFileVersion("23.0.0109.2248")]
 [assembly: AssemblyTitle("Reni Compiler: Library for user interfaces")]
 [assembly: AssemblyCompany("HoyerWare")]
 [assembly: AssemblyProduct("Reni Compiler")]
-[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2022")]
+[assembly: AssemblyCopyright("Copyright © Harald Hoyer 2023")]
 [assembly: InternalsVisibleTo("ReniVSIX")]
 [assembly: InternalsVisibleTo("ReniLSP")]
