7283f6b094296cc1e468a1a59e4c69f7093a8250
Harald Herbert Hoyer <hahoyer@gmail.com>
2023-02-08 12:02:20 +0100

    change: new function

--- src/Reni/Basics/Category.cs
+++ src/Reni/Basics/Category.cs
@@ -67,6 +67,9 @@ public static class CategoryExtension
         return result.Without(Category.Closures);
     }
 
+    internal static Category SmartTyped(this Category category)
+        => category == Category.All? category : category | Category.Type;
+
     internal static bool Contains(this Category container, Category content)
         => (~container & content) == Category.None;
 
