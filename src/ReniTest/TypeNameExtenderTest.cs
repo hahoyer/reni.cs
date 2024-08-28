25197c61949c99fc1bebf719f21db8ec6287ef73
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-08-29 01:52:02 +0200

    refactor: upgrade C#

--- src/ReniTest/TypeNameExtenderTest.cs
+++ src/ReniTest/TypeNameExtenderTest.cs
@@ -1,6 +1,4 @@
 using System.Diagnostics;
-using hw.DebugFormatter;
-using hw.Helper;
 using hw.UnitTest;
 using Reni.FeatureTest.TypeType;
 
@@ -9,21 +7,21 @@ namespace ReniTest;
 [UnitTest]
 sealed class TypeNameExtenderTest
 {
-    [UnitTest]
-    public void TestMethod()
-    {
-        InternalTest(typeof(int), "int");
-        InternalTest(typeof(List<int>), "List<int>", "Generic.List<int>");
-        InternalTest(typeof(List<List<int>>), "List<List<int>>", "Generic.List<Generic.List<int>>");
-        InternalTest(typeof(Dictionary<int, string>), "Dictionary<int,string>", "Generic.Dictionary<int,string>");
-        InternalTest(typeof(TypeOperator), "TypeType.TypeOperator");
-    }
+	[UnitTest]
+	public void TestMethod()
+	{
+		InternalTest(typeof(int), "int");
+		InternalTest(typeof(List<int>), "List<int>", "Generic.List<int>");
+		InternalTest(typeof(List<List<int>>), "List<List<int>>", "Generic.List<Generic.List<int>>");
+		InternalTest(typeof(Dictionary<int, string>), "Dictionary<int,string>", "Generic.Dictionary<int,string>");
+		InternalTest(typeof(TypeOperator), "TypeType.TypeOperator");
+	}
 
-    [DebuggerHidden]
-    static void InternalTest(Type type, params string[] expectedTypeNames)
-        => type
-            .PrettyName()
-            .In(expectedTypeNames)
-            .Assert
-                (() => type + "\nFound   : " + type.PrettyName() + "\nExpected: " + expectedTypeNames, 1);
+	[DebuggerHidden]
+	static void InternalTest(Type type, params string[] expectedTypeNames)
+		=> type
+			.PrettyName()
+			.In(expectedTypeNames)
+			.Assert
+				(() => type + "\nFound   : " + type.PrettyName() + "\nExpected: " + expectedTypeNames, 1);
 }
