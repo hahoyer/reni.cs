d3a6e62a80134e15eef9c3eeb2b9b4d8f89379bf
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-08-29 16:07:31 +0200

    LSPVSIX recreated after accidential delete

---
+++ src/Reni.LSPVSIX/ExtensionEntrypoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+
+namespace Reni.LSPVSIX;
+
+[UsedImplicitly]
+[VisualStudioContribution]
+class ExtensionEntrypoint : Extension
+{
+    public override ExtensionConfiguration ExtensionConfiguration => new()
+    {
+        Metadata = new(
+            "Reni.LSPVSIX",
+            ExtensionAssemblyVersion,
+            "Harald Hoyer",
+            "Reni.LSPVSIX",
+            "Reni Language support") { Icon = "reni.ico" }
+        ,
+    };
+}
---
+++ src/Reni.LSPVSIX/ReniLanguageServerProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.IO.Pipelines;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Editor;
+using Microsoft.VisualStudio.Extensibility.LanguageServer;
+using Microsoft.VisualStudio.RpcContracts.LanguageServerProvider;
+using Nerdbank.Streams;
+using ReniLSP;
+
+namespace Reni.LSPVSIX;
+
+#pragma warning disable VSEXTPREVIEW_LSP
+[VisualStudioContribution]
+[UsedImplicitly]
+public sealed class ReniLanguageServerProvider : LanguageServerProvider
+{
+    [UsedImplicitly]
+    Task? ServerPipeTask;
+
+    [VisualStudioContribution]
+    internal static DocumentTypeConfiguration ReniDocumentType => new("reni")
+    {
+        FileExtensions = new[] { ".reni" }, BaseDocumentType = LanguageServerBaseDocumentType,
+    };
+
+    public ReniLanguageServerProvider
+        (ExtensionCore container, VisualStudioExtensibility extensibilityObject, TraceSource traceSource)
+        : base(container, extensibilityObject) { }
+
+    public override LanguageServerProviderConfiguration LanguageServerProviderConfiguration =>
+        new("Reni Language Server",
+            new[]
+            {
+                DocumentFilter.FromDocumentType("reni"),
+            });
+
+    public override Task<IDuplexPipe?> CreateServerConnectionAsync(CancellationToken token)
+    {
+        var (pipeToLSP, pipeToVS) = FullDuplexStream.CreatePair();
+        var reader = pipeToLSP.UsePipeReader();
+        var writer = pipeToLSP.UsePipeWriter();
+        ServerPipeTask = Task.Run(() => MainContainer.RunServer(reader, writer), token);
+        var duplexPipe = new DuplexPipe(pipeToVS.UsePipeReader(), pipeToVS.UsePipeWriter());
+
+        return Task.FromResult<IDuplexPipe?>(duplexPipe);
+    }
+}
