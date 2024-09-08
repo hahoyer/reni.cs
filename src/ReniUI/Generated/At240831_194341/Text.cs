ba015f9bfd97371dbcc76d81cf317908f97708b5
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-09-08 21:27:40 +0200

    generated tests

--- src/ReniUI/Generated/At240831_194341/Text.cs
+++ src/ReniUI/Generated/At240831_194341/Text.cs
@@ -5,17 +5,23 @@ using Reni.Validation;
 namespace ReniUI.Generated.At240831_194341;
 
 [UnitTest]
-public class Test : CompilerTest
+public sealed class Test : CompilerTest
 {
-    protected override string Target => (SmbFile.SourceFolder / "Text.reni").String;
+    protected override string Target => (SmbFile.SourceFolder / "Test.reni").String;
 
     protected override void Verify(IEnumerable<Issue> issues)
     {
         var issueArray = issues.ToArray();
         var i = 0;
-        //var issueBase = issueArray[i];
-        //(issueBase.IssueId == IssueId.MissingDeclarationValue).Assert(issueBase.Dump);
-        //i++;
+        var issueBase = issueArray[i];
+        (issueBase.IssueId == IssueId.ExtraLeftBracket).Assert(issueBase.Dump);
+        i++;
+        issueBase = issueArray[i];
+        (issueBase.IssueId == IssueId.MissingMatchingRightBracket).Assert(issueBase.Dump);
+        i++;
+        issueBase = issueArray[i];
+        (issueBase.IssueId == IssueId.InvalidExpression).Assert(issueBase.Dump);
+        i++;
         (i == issueArray.Length).Assert();
     }
 }
