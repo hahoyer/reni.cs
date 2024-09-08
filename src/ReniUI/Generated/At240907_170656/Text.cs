812de95f4a0813c7c2789c28495730d679772c64
Harald Herbert Hoyer <hahoyer@gmail.com>
2024-09-08 20:49:52 +0200

    generated tests

---
+++ src/ReniUI/Generated/At240907_170656/Text.cs
@@ -0,0 +1,24 @@
+
+using hw.DebugFormatter;
+using hw.Helper;
+using hw.UnitTest;
+using Reni.FeatureTest.Helper;
+using Reni.Validation;
+
+namespace ReniUI.Generated.At240907_170656;
+
+[UnitTest]
+public sealed class Test : CompilerTest
+{
+    protected override string Target => (SmbFile.SourceFolder / "Test.reni").String;
+
+    protected override void Verify(IEnumerable<Issue> issues)
+    {
+        var issueArray = issues.ToArray();
+        var i = 0;
+        //var issueBase = issueArray[i];
+        //(issueBase.IssueId == IssueId.MissingDeclarationValue).Assert(issueBase.Dump);
+        //i++;
+        (i == issueArray.Length).Assert();
+    }
+}
