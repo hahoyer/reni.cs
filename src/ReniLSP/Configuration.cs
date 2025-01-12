9daf62bd08b8af098191622dcd8b35ac24eb8f9c
Harald Herbert Hoyer <hahoyer@gmail.com>
2025-01-12 21:47:03 +0100

    changes

---
+++ src/ReniLSP/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace ReniLSP;
+
+sealed class FormattingConfiguration : DumpableObject
+{
+	public bool LineBreaksBeforeListToken;
+	public bool AdditionalLineBreaksForMultilineItems = true;
+	public bool LineBreaksAtComplexDeclaration;
+	public int? EmptyLineLimit;
+	public int? MaxLineLength;
+}
