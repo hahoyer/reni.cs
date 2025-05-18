355c590d6330a128f4d6bba62f40f21254135587
Harald Herbert Hoyer <hahoyer@gmail.com>
2025-05-18 13:06:50 +0200

    test

--- src/Reni/MultiSource/MultipleSourcesTest.cs
+++ src/Reni/MultiSource/MultipleSourcesTest.cs
@@ -1,12 +1,23 @@
 using hw.UnitTest;
-using Reni.Context;
 using Reni.FeatureTest.Helper;
+using Reni.FeatureTest.Validation;
+using Reni.Validation;
 
 namespace Reni.MultiSource;
 
 [UnitTest]
 public sealed class MultipleSourcesTest : CompilerTest
 {
+    protected override void Verify(IEnumerable<Issue> issues)
+    {
+        var a = issues.ToArray();
+        var i = 0;
+        a[i++].IsLogDumpLike(23, 21, 23, 27, IssueId.MissingDeclarationInContext, "\"value1\"").Assert();
+        a[i++].IsLogDumpLike(22, 13, 22, 19, IssueId.ConsequenceError, "\"result\"").Assert();
+        a[i++].IsLogDumpLike(23, 13, 23, 19, IssueId.ConsequenceError, "\"result\"").Assert();
+        (a.Length == i).Assert();
+    }
+
     [UnitTest]
     public void FilesSimple()
     {
