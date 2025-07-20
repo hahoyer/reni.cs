7efc3ffc9eaf1d4c177b22686e3a6120264dce0d
Harald Herbert Hoyer <hahoyer@gmail.com>
2025-07-20 15:16:19 +0200

    refactor: reformat

--- src/Reni/Code/Call.cs
+++ src/Reni/Code/Call.cs
@@ -1,25 +1,3 @@
-#region Copyright (C) 2013
-
-//     Project Reni2
-//     Copyright (C) 2011 - 2013 Harald Hoyer
-// 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//     
-//     Comments, bugs and suggestions to hahoyer at yahoo.de
-
-#endregion
-
 using Reni.Basics;
 using Reni.Struct;
 
@@ -33,11 +11,11 @@ sealed class Call : FiberItem
 
     [Node]
     [DisableDump]
-    readonly Size ResultSize;
+    internal readonly Size ArgsAndRefsSize;
 
     [Node]
     [DisableDump]
-    internal readonly Size ArgsAndRefsSize;
+    readonly Size ResultSize;
 
     internal Call(FunctionId functionId, Size resultSize, Size argsAndRefsSize)
     {
@@ -57,7 +35,8 @@ sealed class Call : FiberItem
         where TFiber : default
         => actual.Call(this);
 
-    protected override string GetNodeDump() => base.GetNodeDump() + " FunctionId=" + FunctionId + " ArgsAndRefsSize=" + ArgsAndRefsSize;
+    protected override string GetNodeDump()
+        => base.GetNodeDump() + " FunctionId=" + FunctionId + " ArgsAndRefsSize=" + ArgsAndRefsSize;
 
     internal override void Visit(IVisitor visitor) => visitor.Call(OutputSize, FunctionId, ArgsAndRefsSize);
 
